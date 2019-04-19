da1d193f94f7d0f8f77dab96440c32bbd0dc033a
housesellout <housesellout@gmail.com>
2019-04-19 08:35:29 -0400

    :- missing from previous commit

--- server-data/resources/[esx]/esx_policejob/server/main.lua
+++ server-data/resources/[esx]/esx_policejob/server/main.lua
@@ -45,7 +45,7 @@ AddEventHandler('esx_policejob:confiscatePlayerItem', function(target, itemType,
         sourceXPlayer.addMoney   (amount)
 
         TriggerClientEvent('esx:showNotification', _source, _U('you_confiscated_account', amount, itemName, targetXPlayer.name))
-        TriggerClientEvent('esx:showNotification', target,  _U('you_confiscated_account', amount, itemName, sourceXPlayer.name))
+        TriggerClientEvent('esx:showNotification', target,  _U('got_confiscated_account', amount, itemName, sourceXPlayer.name))
 
 	elseif itemType == 'item_account' then
 		targetXPlayer.removeAccountMoney(itemName, amount)
