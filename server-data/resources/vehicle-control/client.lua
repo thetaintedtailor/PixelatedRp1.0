158870f54cead91ad1cb18a4d2495670f00d7b2c
Kc2693 <kailin.cannon@gmail.com>
2019-04-22 17:20:45 -0600

    add vehicle-control

---
+++ server-data/resources/vehicle-control/client.lua
@@ -0,0 +1,132 @@
+-- C O N F I G --
+interactionDistance = 3.5 --The radius you have to be in to interact with the vehicle.
+lockDistance = 25 --The radius you have to be in to lock your vehicle.
+
+--  V A R I A B L E S --
+engineoff = false
+saved = false
+controlsave_bool = false
+
+-- E N G I N E --
+IsEngineOn = true
+RegisterNetEvent('engine')
+AddEventHandler('engine',function() 
+	local player = GetPlayerPed(-1)
+	
+	if (IsPedSittingInAnyVehicle(player)) then 
+		local vehicle = GetVehiclePedIsIn(player,false)
+		
+		if IsEngineOn == true then
+			IsEngineOn = false
+			SetVehicleEngineOn(vehicle,false,false,false)
+		else
+			IsEngineOn = true
+			SetVehicleUndriveable(vehicle,false)
+			SetVehicleEngineOn(vehicle,true,false,false)
+		end
+		
+		while (IsEngineOn == false) do
+			SetVehicleUndriveable(vehicle,true)
+			Citizen.Wait(0)
+		end
+	end
+end)
+
+RegisterNetEvent('engineoff')
+AddEventHandler('engineoff',function() 
+		local player = GetPlayerPed(-1)
+
+        if (IsPedSittingInAnyVehicle(player)) then 
+            local vehicle = GetVehiclePedIsIn(player,false)
+			engineoff = true
+			ShowNotification("Engine ~r~off~s~.")
+			while (engineoff) do
+			SetVehicleEngineOn(vehicle,false,false,false)
+			SetVehicleUndriveable(vehicle,true)
+			Citizen.Wait(0)
+			end
+		end
+end)
+RegisterNetEvent('engineon')
+AddEventHandler('engineon',function() 
+    local player = GetPlayerPed(-1)
+
+        if (IsPedSittingInAnyVehicle(player)) then 
+            local vehicle = GetVehiclePedIsIn(player,false)
+			engineoff = false
+			SetVehicleUndriveable(vehicle,false)
+			SetVehicleEngineOn(vehicle,true,false,false)
+			ShowNotification("Engine ~g~on~s~.")
+	end
+end)
+-- T R U N K --
+RegisterNetEvent('trunk')
+AddEventHandler('trunk',function() 
+	local player = GetPlayerPed(-1)
+			if controlsave_bool == true then
+				vehicle = saveVehicle
+			else
+				vehicle = GetVehiclePedIsIn(player,true)
+			end
+			
+			local isopen = GetVehicleDoorAngleRatio(vehicle,5)
+			local distanceToVeh = GetDistanceBetweenCoords(GetEntityCoords(player), GetEntityCoords(vehicle), 1)
+			
+			if distanceToVeh <= interactionDistance then
+				if (isopen == 0) then
+				SetVehicleDoorOpen(vehicle,5,0,0)
+				else
+				SetVehicleDoorShut(vehicle,5,0)
+				end
+			else
+				ShowNotification("~r~You must be near your vehicle to do that.")
+			end
+end)
+-- R E A R  D O O R S --
+RegisterNetEvent('rdoors')
+AddEventHandler('rdoors',function() 
+	local player = GetPlayerPed(-1)
+    		if controlsave_bool == true then
+				vehicle = saveVehicle
+			else
+				vehicle = GetVehiclePedIsIn(player,true)
+			end
+			local isopen = GetVehicleDoorAngleRatio(vehicle,2) and GetVehicleDoorAngleRatio(vehicle,3)
+			local distanceToVeh = GetDistanceBetweenCoords(GetEntityCoords(player), GetEntityCoords(vehicle), 1)
+			
+			if distanceToVeh <= interactionDistance then
+				if (isopen == 0) then
+				SetVehicleDoorOpen(vehicle,2,0,0)
+				SetVehicleDoorOpen(vehicle,3,0,0)
+				else
+				SetVehicleDoorShut(vehicle,2,0)
+				SetVehicleDoorShut(vehicle,3,0)
+				end
+			else
+				ShowNotification("~r~You must be near your vehicle to do that.")
+			end
+end)		
+
+-- H O O D --
+RegisterNetEvent('hood')
+AddEventHandler('hood',function() 
+	local player = GetPlayerPed(-1)
+    	if controlsave_bool == true then
+			vehicle = saveVehicle
+		else
+			vehicle = GetVehiclePedIsIn(player,true)
+		end
+			
+			local isopen = GetVehicleDoorAngleRatio(vehicle,4)
+			local distanceToVeh = GetDistanceBetweenCoords(GetEntityCoords(player), GetEntityCoords(vehicle), 1)
+			
+			if distanceToVeh <= interactionDistance then
+				if (isopen == 0) then
+				SetVehicleDoorOpen(vehicle,4,0,0)
+				else
+				SetVehicleDoorShut(vehicle,4,0)
+				end
+			else
+				ShowNotification("~r~You must be near your vehicle to do that.")
+			end
+end)
