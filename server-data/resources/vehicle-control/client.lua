afccdef8ffce69c816286cf997a9ddd915a55e81
Kc2693 <kailin.cannon@gmail.com>
2019-04-22 18:20:10 -0600

    add more car toggle commands

--- server-data/resources/vehicle-control/client.lua
+++ server-data/resources/vehicle-control/client.lua
@@ -129,3 +129,119 @@ RegisterCommand('carshow', function()
 		ESX.ShowNotification('You must be the driver of a vehicle to use this.')
     end
 end)
+
+
+-- INDIVIDUAL DOOR CONTROLS --
+
+RegisterCommand('door1', function()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+		local frontLeftDoor = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'door_dside_f')
+		if frontLeftDoor ~= -1 then
+			if GetVehicleDoorAngleRatio(vehicle, 0) > 0.0 then 
+				SetVehicleDoorShut(vehicle, 0, false)            
+			else
+				SetVehicleDoorOpen(vehicle, 0, false)             
+			end
+		else
+			ESX.ShowNotification('This vehicle does not have a front driver-side door.')
+		end
+	else
+		ESX.ShowNotification('You must be the driver of a vehicle to use this.')
+    end
+end)
+
+RegisterCommand('door2', function()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+		local frontRightDoor = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'door_pside_f')
+		if frontRightDoor ~= -1 then
+			if GetVehicleDoorAngleRatio(vehicle, 1) > 0.0 then 
+				SetVehicleDoorShut(vehicle, 1, false)            
+			else
+				SetVehicleDoorOpen(vehicle, 1, false)             
+			end
+		else
+			ESX.ShowNotification('This vehicle does not have a front passenger-side door.')
+		end
+	else
+		ESX.ShowNotification('You must be the driver of a vehicle to use this.')
+    end
+end)
+
+RegisterNUICallback('door3', function()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+		local rearLeftDoor = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'door_dside_r')
+		if rearLeftDoor ~= -1 then
+			if GetVehicleDoorAngleRatio(vehicle, 2) > 0.0 then 
+				SetVehicleDoorShut(vehicle, 2, false)            
+			else
+				SetVehicleDoorOpen(vehicle, 2, false)             
+			end
+		else
+			ESX.ShowNotification('This vehicle does not have a rear driver-side door.')
+		end
+	else
+		ESX.ShowNotification('You must be the driver of a vehicle to use this.')
+    end
+end)
+
+RegisterNUICallback('door4', function()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+		local rearRightDoor = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'door_pside_r')
+		if rearRightDoor ~= -1 then
+			if GetVehicleDoorAngleRatio(vehicle, 3) > 0.0 then 
+				SetVehicleDoorShut(vehicle, 3, false)            
+			else
+				SetVehicleDoorOpen(vehicle, 3, false)             
+			end
+		else
+			ESX.ShowNotification('This vehicle does not have a rear passenger-side door.')
+		end
+	else
+		ESX.ShowNotification('You must be the driver of a vehicle to use this.')
+    end
+end)
+
+-- ALL WINDOWS --
+local windowsUp = true
+RegisterCommand('windows', function()
+    local vehicle = GetVehiclePedIsIn(PlayerPedId(), false)
+    if vehicle ~= nil and vehicle ~= 0 and GetPedInVehicleSeat(vehicle, 0) then
+		local frontLeftWindow = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'window_lf')
+		local frontRightWindow = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'window_rf')
+		local rearLeftWindow = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'window_lr')
+		local rearRightWindow = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'window_rr')
+		local frontMiddleWindow = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'window_lm')
+		local rearMiddleWindow = GetEntityBoneIndexByName(GetVehiclePedIsIn(GetPlayerPed(-1), false), 'window_rm')
+		
+		if frontLeftWindow ~= -1 or frontRightWindow ~= -1 or rearLeftWindow ~= -1 or rearRightWindow ~= -1 or frontMiddleWindow ~= -1 or rearMiddleWindow ~= -1 then
+			if windowsUp == true then
+				RollDownWindow(vehicle, 0)
+				RollDownWindow(vehicle, 1)
+				RollDownWindow(vehicle, 2)
+				RollDownWindow(vehicle, 3)
+				RollDownWindow(vehicle, 4)
+				RollDownWindow(vehicle, 5)
+				windowsUp = false
+			else if windowsUp == false then
+				RollUpWindow(vehicle, 0)
+				RollUpWindow(vehicle, 1)
+				RollUpWindow(vehicle, 2)
+				RollUpWindow(vehicle, 3)
+				RollUpWindow(vehicle, 4)
+				RollUpWindow(vehicle, 5)
+				windowsUp = true
+			end
+		else
+			ESX.ShowNotification('This vehicle has no windows.')
+		end
+	else
+		ESX.ShowNotification('You must be the driver of a vehicle to use this.')
+	end
+end)
+
+
+
