0fcac83b7110b0582e5ab565ba58c8c474f21223
Kailin Cannon <kailin.cannon@gmail.com>
2019-04-23 17:56:34 -0600

    change height of esx voice

--- server-data/resources/[esx]/esx_voice/client/main.lua
+++ server-data/resources/[esx]/esx_voice/client/main.lua
@@ -24,7 +24,7 @@ function drawLevel(r, g, b, a)
 
 	BeginTextCommandDisplayText("STRING")
 	AddTextComponentSubstringPlayerName(_U('voice', voice.level))
-	EndTextCommandDisplayText(0.175, 0.856)
+	EndTextCommandDisplayText(0.175, 0.857)
 end
 
 AddEventHandler('onClientMapStart', function()
