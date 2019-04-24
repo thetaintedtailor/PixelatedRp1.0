1adab5c7c2e54b68db069917c205df892afcbc98
Kc2693 <kailin.cannon@gmail.com>
2019-04-24 13:27:32 -0600

    fix hospital

--- server-data/resources/bob74_ipl/gtav/base.lua
+++ server-data/resources/bob74_ipl/gtav/base.lua
@@ -74,7 +74,9 @@ Citizen.CreateThread(function()
     RequestIpl("CanyonRvrShallow")
         
     -- Pillbox hospital: 307.1680, -590.807, 43.280
-    RequestIpl("rc12b_default")
+    --RequestIpl("rc12b_default")
+	RemoveIpl("rc12b_default")
+	RequestIpl("rc12b_hospitalinterior") 
         
     -- Josh's house: -1117.1632080078, 303.090698, 66.52217
     RequestIpl("bh1_47_joshhse_unburnt")
