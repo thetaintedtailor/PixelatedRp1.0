21018aa5829ae7e69a811672d9411bac1cbd4de7
Kc2693 <kailin.cannon@gmail.com>
2019-04-23 20:07:00 -0600

    add esx_coffee

--- server-data/resources/[esx]/esx_basicneeds/client/main.lua
+++ server-data/resources/[esx]/esx_basicneeds/client/main.lua
@@ -141,3 +141,26 @@ AddEventHandler('esx_basicneeds:onDrink', function(prop_name)
 
 	end
 end)
+
+RegisterNetEvent('esx_basicneeds:onDrinkCoffee')
+AddEventHandler('esx_basicneeds:onDrinkCoffee', function(prop_name)
+	if not IsAnimated then
+		local prop_name = prop_name or 'p_amb_coffeecup_01'
+		IsAnimated = true
+		local playerPed = GetPlayerPed(-1)
+		Citizen.CreateThread(function()
+			local x,y,z = table.unpack(GetEntityCoords(playerPed))
+			prop = CreateObject(GetHashKey(prop_name), x, y, z+0.2,  true,  true, true)
+	        AttachEntityToEntity(prop, playerPed, GetPedBoneIndex(playerPed, 18905), 0.12, 0.028, 0.001, 10.0, 175.0, 0.0, true, true, false, true, 1, true)
+			RequestAnimDict('mp_player_intdrink')
+			while not HasAnimDictLoaded('mp_player_intdrink') do
+				Wait(0)
+			end
+			TaskPlayAnim(playerPed, 'mp_player_intdrink', 'loop_bottle', 1.0, -1.0, 2000, 0, 1, true, true, true)
+			Wait(3000)
+	        IsAnimated = false
+	        ClearPedSecondaryTask(playerPed)
+			DeleteObject(prop)
+		end)
+	end
+end)
--- server-data/resources/[esx]/esx_basicneeds/locales/en.lua
+++ server-data/resources/[esx]/esx_basicneeds/locales/en.lua
@@ -1,4 +1,5 @@
 Locales['en'] = {
-	['used_bread'] = 'you have used ~y~1x~s~ ~b~bread~s~',
-	['used_water'] = 'you have used ~y~1x~s~ ~b~water~s~',
+	['used_bread'] = 'you eat some ~y~1x~s~ ~b~bread~s~',
+	['used_water'] = 'you drink some ~y~1x~s~ ~b~water~s~',
+	['used_coffee'] = 'you drink a nice hot cup of coffee'
 }
