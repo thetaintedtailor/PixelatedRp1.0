e888cb247b0b696a6b58f2916964fff15e91ed53
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-04-25 00:08:36 -0600

    adjusted payout

--- server-data/resources/[esx]/esx_vangelico_robbery/config.lua
+++ server-data/resources/[esx]/esx_vangelico_robbery/config.lua
@@ -7,7 +7,7 @@ Config.RequiredCopsSell = 1
 Stores = {
 	["jewelry"] = {
 		position = { ['x'] = -629.99, ['y'] = -236.542, ['z'] = 38.05 },       
-		reward = math.random(100000,150000),
+		reward = math.random(50000,75000),
 		nameofstore = "jewelry",
 		lastrobbed = 0
 	}
