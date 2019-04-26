b9ee05075dd97e46b588dd37cc21f3979532fb22
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-04-26 00:04:48 -0600

    changed language

--- server-data/resources/[esx]/esx_duty/config.lua
+++ server-data/resources/[esx]/esx_duty/config.lua
@@ -1,7 +1,7 @@
 Config                            = {}
 Config.DrawDistance               = 100.0
 --language currently available EN and SV
-Config.Locale                     = 'sv'
+Config.Locale                     = 'en'
 
 Config.Zones = {
 
