6babf3f9546062a20ab34dcb8237711c691105e4
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-04-26 00:16:34 -0600

    changed emter to enter

--- server-data/resources/[esx]/esx_duty/translation/en.lua
+++ server-data/resources/[esx]/esx_duty/translation/en.lua
@@ -1,6 +1,6 @@
 Locales['en'] = {
   -- regulars
-  	['duty'] = 'Press ~INPUT_CONTEXT~ to ~g~emter~s~/~r~exit~s~ duty',
+  	['duty'] = 'Press ~INPUT_CONTEXT~ to ~g~enter~s~/~r~exit~s~ duty',
 	['onduty'] = 'You went onduty.',
 	['offduty'] = 'You went offduty.',
 	['notpol'] = 'You are not a policemen.',
