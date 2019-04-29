3e42d8716d7b9b7e019efba1e5584057e6fbf4e5
house <housesellout@gmail.com>
2019-04-29 13:50:11 -0400

    :- updated loading screen music to kat’s original no doubt (as mp3 this time) 	- deleted old .ogg files, replaced with .mp3 files 	- misc code cleanup

--- server-data/resources/vk_loadingscreen/config.js
+++ server-data/resources/vk_loadingscreen/config.js
@@ -37,14 +37,17 @@ VK.tips.list = [ // Tips list
     "Around here we call Kat the Fastest Dev in the West",
 ];
 
-VK.music.url = "music/macklemore.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
+
 VK.music.volume = 0.1; // Music volume (0-1)
-VK.music.title = "And We Danced - Macklemore"; // Music title ("NONE" to desactive)
-VK.music.submitedBy = "suggested by 40u53"; // Music submited by... ("NONE" to desactive)
-//VK.music.title = "Paper Planes - M.I.A"; // Music title ("NONE" to desactive)
+
+VK.music.url = "music/nodoubt-hellagood.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
+VK.music.title = "No Doubt - Hella Good"; // Music title ("NONE" to desactive)
+VK.music.submitedBy = "suggested by Kat"; // Music submited by... ("NONE" to desactive)
+
+//VK.music.url = "music/blink182-damnit.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
+//VK.music.title = "Damnit - Blink-182"; // Music title ("NONE" to desactive)
 //VK.music.submitedBy = "suggested by h0u53"; // Music submited by... ("NONE" to desactive)
-//VK.music.title = "Hella Good - No Doubt"; // Music title ("NONE" to desactive)
-//VK.music.submitedBy = "suggested by Kat"; // Music submited by... ("NONE" to desactive)
+
 
 VK.info.logo = "https://i.postimg.cc/tCWn8Scx/pixelated.png"; // Logo ("NONE" to desactive)
 VK.info.text = "NONE"; // Bottom right corner text ("NONE" to desactive)
