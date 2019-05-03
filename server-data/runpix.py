79a97c9c44c8aff1fbf85dc8b48b71bed3c89e41
house <housesellout@gmail.com>
2019-05-03 17:56:54 -0400

    :- updated runpix.py (‘help’ printouts, filename, added ‘-nat’ option) 	- testing to see if has production effect (before merging to master)

--- server-data/runpix.py
+++ server-data/runpix.py
@@ -1,16 +1,17 @@
-print('GO runpix.py -> starting IMPORTs')
+print('GO runfivem.py -> starting IMPORTs')
 import sys
 import subprocess
 #subprocess.call(["ls","-la"])
 
 cStrDivider = '#----------------------------------------------------------------------------------------------------#'
-filename = 'runpix.py'
+filename = 'runfivem.py'
 print("\n IMPORTs complete:- STARTING -> file '%s' . . . " % filename)
 
 ## run.sh path
-strPath = '/srv/PixelatedRp1.0/server/run.sh'
+strPath = '/srv/fivem-dm4c/server/run.sh'
 
 ## .cfg file names
+strCfgNat = 'nat_server.cfg'
 strCfgDev = 'dev_server.cfg'
 strCfgProd = 'server.cfg'
 
@@ -39,11 +40,11 @@ usage = (":- USAGE examples...                          \n\n"
          " *** IMPORT ***                               \n\n"
          "  must run from /srv/<project>/server-data/   \n\n"
          " *** IMPORT ***                               \n\n"
-         "1) ‘$ python3 runpix.py -fivem'                 \n"
+         "1) ‘$ python3 runfivem.py -fivem'                 \n"
          "      - runs tmux (terminal multiplexar) for maintainig fivem PID after ssh HUP/disconnect \n"
          "          - after tmux terminal is launched, then run...                \n"
-         "          -  '$ python3 runpix.py -dev|prod'                          \n\n"
-         "1a) ‘$ python3 runpix.py -fivem-join'                                   \n"
+         "          -  '$ python3 runfivem.py -dev|prod'                          \n\n"
+         "1a) ‘$ python3 runfivem.py -fivem-join'                                   \n"
          "      - re-join (attach) to current 'fivem' session                     \n"
          "           (runs: '$ tmux a -t fivem')                                  \n"
          "      - utilized for reviewing live logs, hot-resets, etc.            \n\n"
@@ -52,33 +53,44 @@ usage = (":- USAGE examples...                          \n\n"
          "          # detach (leave) current session                                  \n"
          "              > ctl+b , d                                                   \n"
          "          # KILL current session (this will shutdown the fivem server)      \n"
-         "              > ctl+d                                                     \n\n"
-         "1b) ‘$ python3 runpix.py -fivem-kill'                                   \n"
+         "              > ctl+d                                                         \n"
+         "          # Enter scrolling mode                                              \n"
+         "              > ctl+d , [                                                     \n"
+         "          # Exit scrolling mode                                              \n"
+         "              > q                                                            \n\n"
+         "1b) ‘$ python3 runfivem.py -fivem-kill'                                   \n"
          "      - kill (shutdown) the current 'fivem' server session              \n"
          "           (runs: '$ tmux kill-ses -t fivem')                           \n"
          "      - NOTE: can also kill with hot key controls (above)             \n\n"
-         "2) ‘$ python3 runpix.py -dev \n"
+         "2) ‘$ python3 runfivem.py -nat \n"
+         "      - runs nat_server.cfg \n"
+         "          - uses custom 'sv_licenseKey' \n"
+         "          - uses current IP allocation (0.0.0.0) \n"
+         "          - uses default listening port (0.0.0.0:30120) \n\n"
+         "3) ‘$ python3 runfivem.py -dev \n"
          "      - runs dev_server.cfg \n"
          "          - uses custom 'sv_licenseKey' \n"
          "          - uses current IP allocation (0.0.0.0) \n"
          "          - uses default listening port (0.0.0.0:30120) \n\n"
-         "3) ‘$ python3 runpix.py -prod \n"
+         "4) ‘$ python3 runfivem.py -prod \n"
          "      - runs server.cfg \n"
          "          - uses custom 'sv_licenseKey' \n"
          "          - uses current IP allocation (0.0.0.0) \n"
          "          - uses default listening port (0.0.0.0:30120) \n"
          "          - NOTE: '-prod' flag enabled 042319 \n\n"
-         "4) ‘$ python3 runpix.py -zap \n"
+         "5) ‘$ python3 runfivem.py -zap \n"
          "      - runs server.cfg \n"
          "          - uses no 'sv_licenseKey' var set \n"
          "          - uses dynamic zap IP:port assisgnment (last 185.249.196.40:32070) \n"
          "          - WARNING: '-zap' flag expects yield error ref: no 'sv_licenseKey' \n\n"
-         "5) examples... \n"
-         "      - '$ python runpix.py -fivem'   \n"
-         "      - '$ python runpix.py -fivem-join'   \n"
-         "      - '$ python runpix.py -dev'     \n"
-         "      - '$ python runpix.py -prod'    \n"
-         "      - '$ python runpix.py -zap'     \n"
+         "6) examples... \n"
+         "      - '$ python runfivem.py -fivem'   \n"
+         "      - '$ python runfivem.py -fivem-join'   \n"
+         "      - '$ python runfivem.py -fivem-kill'   \n"
+         "      - '$ python runfivem.py -nat'     \n"
+         "      - '$ python runfivem.py -dev'     \n"
+         "      - '$ python runfivem.py -prod'    \n"
+         "      - '$ python runfivem.py -zap'     \n"
          "      - NOTE: '-prod' flag currently enabled as of 042319 \n"
          "      - WARNING: '-zap' flag expects yield error ref: no 'sv_licenseKey' \n\n"
          "      - . . . \n\n"
@@ -117,10 +129,14 @@ if argCnt > 1:
             sys.exit()
 
 
+        if argv == '-nat':
+            print("\n '-nat' flag detected ... setting %s... (%s)" % (strCfgNat,filename))
+            stringConfig = strCfgNat
+        
         if argv == '-dev':
             print("\n '-dev' flag detected ... setting %s... (%s)" % (strCfgDev,filename))
             stringConfig = strCfgDev
-        
+
         if argv == '-prod':
             print("\n '-prod' flag detected ... setting %s... (%s)" % (strCfgProd,filename))
             stringConfig = strCfgProd
