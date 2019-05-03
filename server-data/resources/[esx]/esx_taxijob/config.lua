935c7cf23179324ab57857063de92b8840d9f2be
Kc2693 <kailin.cannon@gmail.com>
2019-05-03 10:35:43 -0600

    fix syntax errors

--- server-data/resources/[esx]/esx_taxijob/config.lua
+++ server-data/resources/[esx]/esx_taxijob/config.lua
@@ -53,7 +53,7 @@ Config.Zones = {
 		Size  = {x = 1.0, y = 1.0, z = 1.0},
 		Color = {r = 204, g = 204, b = 0},
 		Type  = 20, Rotate = true
-	--},
+	},
 
 	Cloakroom = {
 		Pos     = {x = 894.88, y = -180.23, z = 74.5},
