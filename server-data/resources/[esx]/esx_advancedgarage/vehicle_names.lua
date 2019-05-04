f2c3b1b10ee384bb758d9f2c12ab7ceaaedaf566
Kc2693 <kailin.cannon@gmail.com>
2019-05-04 16:05:50 -0600

    change location of police vehicle names

--- server-data/resources/[esx]/esx_advancedgarage/vehicle_names.lua
+++ server-data/resources/[esx]/esx_advancedgarage/vehicle_names.lua
@@ -14,18 +14,4 @@ Citizen.CreateThread(function()
     AddTextEntry('rs5r', 'Audi RS 5'),
     AddTextEntry('rs6', 'Audi RS 6'),
     AddTextEntry('SSTI', 'Subaru WRX STI'),
-
-    --Police cars
-    AddTextEntry('R 1200RT', 'BMW R1200RT'),
-    AddTextEntry('SRT8', 'Jeep SRT8'),
-    AddTextEntry('Mustang', "'15 Mustang GT'"),
-    AddTextEntry('SFTSU', 'Ranger'),
-    AddTextEntry('SFBC1', 'SUV'),
-    AddTextEntry('SFBC2', 'SUV2'),
-    AddTextEntry('SFBC3', 'SUV3'),
-    AddTextEntry('SFBC4', 'SFBC4'),
-    AddTextEntry('SFBC5', 'SFBC5'),
-    AddTextEntry('SFUM1', 'SFUM1'),
-    AddTextEntry('SFUM2', 'SFUM2'),
-    AddTextEntry('SFUM1', 'SFUM1'),
 end)
