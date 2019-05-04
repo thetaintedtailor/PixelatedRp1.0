c0cee91469197bdbc1594077c7626c3c2c839489
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-05-04 10:21:11 -0600

    added webhook

--- server-data/resources/[esx]/esx_discord_bot/config.lua
+++ server-data/resources/[esx]/esx_discord_bot/config.lua
@@ -6,18 +6,18 @@ Config.red 					  = 16711680
 Config.orange 				  = 16744192
 Config.blue 				  = 2061822
 Config.purple 				  = 11750815
-Config.webhook                = "PASTE_HERE_YOUR_WEBHOOK"
+Config.webhook                = "https://discordapp.com/api/webhooks/574268422171525120/5iM1NElmdDWIHKDd50my6JYBQsw2HtI94elacfKIDonD1r5Qf7dCAj16XE10CsxBoA1d"
 
 
 settings = {
 	LogKills = true, -- Log when a player kill an other player.
-	LogEnterPoliceVehicle = true, -- Log when an player enter in a police vehicle.
-	LogEnterBlackListedVehicle = true, -- Log when a player enter in a blacklisted vehicle.
-	LogPedJacking = true, -- Log when a player is jacking a car
+	LogEnterPoliceVehicle = false, -- Log when an player enter in a police vehicle.
+	LogEnterBlackListedVehicle = false, -- Log when a player enter in a blacklisted vehicle.
+	LogPedJacking = false, -- Log when a player is jacking a car
 	LogChatServer = true, -- Log when a player is talking in the chat , /command works too.
-	LogLoginServer = true, -- Log when a player is connecting/disconnecting to the server.
-	LogItemTransfer = true, -- Log when a player is giving an item.
-	LogWeaponTransfer = true, -- Log when a player is giving a weapon.
+	LogLoginServer = false, -- Log when a player is connecting/disconnecting to the server.
+	LogItemTransfer = false, -- Log when a player is giving an item.
+	LogWeaponTransfer = false, -- Log when a player is giving a weapon.
 	LogMoneyTransfer = true, -- Log when a player is giving money
 	LogMoneyBankTransfert = true, -- Log when a player is giving money from bankaccount
 
