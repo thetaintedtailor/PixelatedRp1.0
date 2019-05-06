8df7b262f0137c8bdb9eab530c82adc3a92e183e
Kc2693 <kailin.cannon@gmail.com>
2019-05-06 01:28:12 -0600

    change clothes shop menu alignment

--- server-data/resources/[esx]/esx_clotheshop/client/main.lua
+++ server-data/resources/[esx]/esx_clotheshop/client/main.lua
@@ -34,7 +34,7 @@ function OpenShopMenu()
 		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'shop_confirm',
 		{
 			title = _U('valid_this_purchase'),
-			align = 'left',
+			align = 'right',
 			elements = {
 				{label = _U('no'), value = 'no'},
 				{label = _U('yes'), value = 'yes'}
