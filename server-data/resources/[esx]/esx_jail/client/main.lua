d3e9d1be382fc4defefa48656bc422032f3f1f35
Kc2693 <kailin.cannon@gmail.com>
2019-05-05 23:29:48 -0600

    jail hud change

--- server-data/resources/[esx]/esx_jail/client/main.lua
+++ server-data/resources/[esx]/esx_jail/client/main.lua
@@ -88,7 +88,7 @@ Citizen.CreateThread(function()
 				fastTimer = JailTime
 			end
 
-			draw2dText(_U('remaining_msg', ESX.Math.Round(fastTimer)), { 0.175, 1.255 } )
+			draw2dText(_U('remaining_msg', ESX.Math.Round(fastTimer)), { 0.175, 0.655 } )
 			fastTimer = fastTimer - 0.01
 		else
 			Citizen.Wait(1000)
