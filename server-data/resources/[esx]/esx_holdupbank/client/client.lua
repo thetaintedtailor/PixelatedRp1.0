99347d09ad5fd3552059f9cc59eb864ab04f10cf
Kc2693 <kailin.cannon@gmail.com>
2019-05-05 23:55:44 -0600

    bank holdup ui changes

--- server-data/resources/[esx]/esx_holdupbank/client/client.lua
+++ server-data/resources/[esx]/esx_holdupbank/client/client.lua
@@ -129,7 +129,7 @@ Citizen.CreateThread(function()
 
 		if holdingup then
 
-			drawTxt(0.66, 1.44, 1.0,1.0,0.4, _U('robbery_of') .. secondsRemaining .. _U('seconds_remaining'), 255, 255, 255, 255)
+			drawTxt(0.66, 1.44, 1.0,1.15,0.4, _U('robbery_of') .. secondsRemaining .. _U('seconds_remaining'), 255, 255, 255, 255)
 
 			local pos2 = Banks[bank].position
 
