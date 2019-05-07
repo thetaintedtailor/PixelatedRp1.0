89aff3eb05c249790bfec0ef13f5a74153348c24
house <housesellout@gmail.com>
2019-05-06 23:09:24 -0400

    :- re-enabled debug logging with rcon print

--- server-data/resources/[esx]/esx_policejob/client/main.lua
+++ server-data/resources/[esx]/esx_policejob/client/main.lua
@@ -905,9 +905,10 @@ function OpenPoliceActionsMenu()
 end
 
 function OpenIdentityCardMenu(player)
-    -- local funcname = "[" .. os.time() .. " OpenIdentityCardMenu]"
+    local funcname = "[" .. os.time() .. " OpenIdentityCardMenu]"
 	ESX.TriggerServerCallback('esx_policejob:getOtherPlayerData', function(data)
-        -- print(string.format("%s data.name: %s", funcname, data.name))
+        print(string.format("%s data.name: %s", funcname, data.name))
+        RconPrint(string.format("%s data.name: %s", funcname, data.name))
         -- print(string.format("%s data.firstname: %s; data.lastname: %s", funcname, data.firstname, data.lastname))
         -- print(string.format("%s data: %s", funcname, data))
 		local elements    = {}
