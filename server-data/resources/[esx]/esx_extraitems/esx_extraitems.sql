6c769d7339199e16a7964f0e03c364097fce1b91
Kc2693 <kailin.cannon@gmail.com>
2019-05-07 15:22:37 -0600

    add mod, remove unwanted items

---
+++ server-data/resources/[esx]/esx_extraitems/esx_extraitems.sql
@@ -0,0 +1,30 @@
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('picnic', 'Picknick kit', 1),
+	('donut', 'Munk', 6),
+	('oxygen_mask', 'Dykmask', 2),
+	('sandwich', 'Macka', 5),
+	('rose', 'Röd ros', 1),
+	('redgull', 'Redgull', 4)
+;
+
+INSERT INTO `shops` (name, item, price) VALUES
+
+	('LTDgasoline', 'picnic', 500),
+	('LTDgasoline', 'donut', 20),
+	('LTDgasoline', 'oxygen_mask', 6500),
+	('LTDgasoline', 'sandwich', 30),
+	('LTDgasoline', 'rose', 55),
+	('LTDgasoline', 'redgull', 2000),
+	('TwentyFourSeven', 'picnic', 500),
+	('TwentyFourSeven', 'donut', 20),
+	('TwentyFourSeven', 'oxygen_mask', 6500),
+	('TwentyFourSeven', 'sandwich', 30),
+	('TwentyFourSeven', 'rose', 55),
+	('TwentyFourSeven', 'redgull', 2000),
+	('RobsLiquor', 'picnic', 500),
+	('RobsLiquor', 'donut', 20),
+	('RobsLiquor', 'oxygen_mask', 6500),
+	('RobsLiquor', 'sandwich', 30),
+	('RobsLiquor', 'rose', 55),
+	('RobsLiquor', 'redgull', 2000)
+;
