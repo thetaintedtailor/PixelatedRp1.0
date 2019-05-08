da25b6468909c99a3dcaf57dff804ef6b0ec4925
WertyCx <jesse_anderson1993@hotmail.com>
2019-05-07 19:55:39 -0500

    test

--- server-data/resources/[esx]/esx_AdvancedFishing/client.lua
+++ server-data/resources/[esx]/esx_AdvancedFishing/client.lua
@@ -134,6 +134,7 @@ Citizen.CreateThread(function()
 				ESX.ShowNotification("~r~Stopped fishing")
 			end
 			if fishing then
+				BlockWeaponWheelThisFrame()
 				playerPed = GetPlayerPed(-1)
 				local pos = GetEntityCoords(GetPlayerPed(-1))
 				if GetWaterHeight(pos.x, pos.y, pos.z-0.0, pos.z-0.5) then
