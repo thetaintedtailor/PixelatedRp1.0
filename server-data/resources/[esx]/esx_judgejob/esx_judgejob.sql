f42bb48494236d0eb05a94ff1946172d92d0cb7d
Kc2693 <kailin.cannon@gmail.com>
2019-05-08 13:34:37 -0600

    add judgejob

---
+++ server-data/resources/[esx]/esx_judgejob/esx_judgejob.sql
@@ -0,0 +1,5 @@
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+(80, 'judge', 0, 'boss', 'Judge', 300, '', '');
+
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+('judge', 'City Judge', 1);
