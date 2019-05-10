93eafff55a31e6c920b17794c22773b0c79e413f
Kc2693 <kailin.cannon@gmail.com>
2019-05-10 12:56:48 -0600

    fix song title for loading screen

--- server-data/resources/vk_loadingscreen/config.js
+++ server-data/resources/vk_loadingscreen/config.js
@@ -40,8 +40,8 @@ VK.tips.list = [ // Tips list
 
 VK.music.volume = 0.1; // Music volume (0-1)
 VK.music.url = "music/ManilaGrey-FriendsofFriends.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
-VK.music.title = "Why dont you do right - Jessica Rabbit"; // Music title ("NONE" to desactive)
-VK.music.submitedBy = "suggested by DeludedWench(Rori)"; // Music submited by... ("NONE" to desactive)
+VK.music.title = "Friends of Friends - Manila Grey"; // Music title ("NONE" to desactive)
+VK.music.submitedBy = "suggested by playskooTV (Nick Drippin)"; // Music submited by... ("NONE" to desactive)
 
 VK.info.logo = "https://i.postimg.cc/tCWn8Scx/pixelated.png"; // Logo ("NONE" to desactive)
 VK.info.text = "NONE"; // Bottom right corner text ("NONE" to desactive)
