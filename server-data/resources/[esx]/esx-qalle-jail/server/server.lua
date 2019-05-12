4a512acc0e9b3dcf53092e430dd49e8b0fe3c283
WertyCx <jesse_anderson1993@hotmail.com>
2019-05-12 17:45:18 -0500

    attempt new jailtime reduce for work fix?

--- server-data/resources/[esx]/esx-qalle-jail/server/server.lua
+++ server-data/resources/[esx]/esx-qalle-jail/server/server.lua
@@ -102,9 +102,18 @@ AddEventHandler("esx-qalle-jail:prisonWorkReward", function()
 
 	xPlayer.addMoney(math.random(13, 21))
 
-	EditJailTime(jailPlayer, -1)
-
 	TriggerClientEvent("esx:showNotification", src, "Thanks, here you have som cash for food!")
+
+	while jailTime > 0 do
+
+		jailTime = jailTime - 1
+
+		ESX.ShowNotification("You have " .. jailTime .. " minutes left in jail!")
+
+		TriggerServerEvent("esx-qalle-jail:updateJailTime", jailTime)
+
+	end	
+
 end)
 
 function JailPlayer(jailPlayer, jailTime)
