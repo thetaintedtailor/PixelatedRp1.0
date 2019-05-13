65f43d6bdc527e1834a80189a17cbac3b3c52c2b
WertyCx <jesse_anderson1993@hotmail.com>
2019-05-13 02:07:00 -0500

    shrink marker sizwe

---
+++ server-data/resources/[esx]/esx_carwash/washconfig.lua
@@ -0,0 +1,82 @@
+Config = {}
+
+-- Carwash Markers 
+Config.zones = {
+	
+	Carwash1 = {           
+		x = 26.49,
+		y = -1391.95,
+		z = 28.86,
+		w = 1.0,
+		h = 1.0,
+		visible = true,
+		t = 36,          
+		color = {
+			r = 0,
+			g = 0,
+			b = 102
+		}
+		
+	},
+	
+	Carwash2 = {         
+		x = 169.02,
+		y = -1720,
+		z = 28.79,
+		w = 1.0,
+		h = 1.0,
+		visible = true,
+		t = 36,
+		color = {
+			r = 0,
+			g = 0,
+			b = 102
+		}
+		
+	},
+	
+	Carwash3 = {
+		x = 74.27,
+		y = 6427.65,
+		z = 30.94,
+		w = 1.0,
+		h = 1.0,		
+		visible = true,
+		t = 36,
+		color = {
+			r = 0,
+			g = 0,
+			b = 102
+		}
+	},
+	
+	Carwash4 = {
+		x = -699.63,
+		y = -932.75,
+		z = -18.51,
+		w = 1.0,
+		h = 1.0,		
+		visible = true,
+		t = 36,
+		color = {
+			r = 0,
+			g = 0,
+			b = 102
+		}
+	},
+
+	Carwash5 = {
+		x = 1362.58,
+		y = 3592.11,
+		z = 34.42,
+		w = 1.0,
+		h = 1.0,		
+		visible = true,
+		t = 36,
+		color = {
+			r = 0,
+			g = 0,
+			b = 102
+		}
+	}
+} 
