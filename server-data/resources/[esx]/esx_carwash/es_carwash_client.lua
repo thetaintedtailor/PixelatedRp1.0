f7e51a5adae8f3af738c0d9f472a0de318502346
WertyCx <jesse_anderson1993@hotmail.com>
2019-05-13 00:58:23 -0500

    re edit marker

--- server-data/resources/[esx]/esx_carwash/es_carwash_client.lua
+++ server-data/resources/[esx]/esx_carwash/es_carwash_client.lua
@@ -40,7 +40,7 @@ Citizen.CreateThread(function ()
 		if IsPedSittingInAnyVehicle(GetPlayerPed(-1)) then 
 			for i = 1, #vehicleWashStation do
 				garageCoords2 = vehicleWashStation[i]
-				DrawMarker(36, garageCoords2[1], garageCoords2[2], garageCoords2[3], 0, 0, 0, 0, 0, 0, 2.0, 2.0, 2.0, 0, 157, 0, 155, 0, 0, 2, 0, 0, 0, 0)
+				DrawMarker(36, garageCoords2[1], garageCoords2[2], garageCoords2[3], 0, 0, 0, 0, 0, 0, 5.0, 5.0, 2.0, 0, 157, 0, 155, 0, 0, 2, 0, 0, 0, 0)
 				if GetDistanceBetweenCoords(GetEntityCoords(GetPlayerPed(-1)), garageCoords2[1], garageCoords2[2], garageCoords2[3], true ) < 5 then
 					es_carwash_DrawSubtitleTimed("Press [~g~ E ~s~] to wash your vehicle!")
 					if IsControlJustPressed(1, Key) then
