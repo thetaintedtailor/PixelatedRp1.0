eb736a6e2c0ec6b3179c0e81f9373425ac216d5b
WertyCx <jesse_anderson1993@hotmail.com>
2019-05-13 02:03:02 -0500

    add markers to carwash

--- server-data/resources/[esx]/esx_carwash/__resource.lua
+++ server-data/resources/[esx]/esx_carwash/__resource.lua
@@ -2,5 +2,13 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 description 'EssentialMode 5 CarWash by TheSpartaPT.'
 
-client_script 'es_carwash_client.lua'
 server_script 'es_carwash_server.lua'
+
+
+client_scripts {
+
+    'config.lua',
+    'client.lua',
+    'es_carwash_client.lua'
+  
+  }
