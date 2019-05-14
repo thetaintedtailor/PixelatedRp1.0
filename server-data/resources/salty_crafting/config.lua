fcfba0b159e660bd697ce37216599b97267dc7d5
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-05-14 13:48:10 -0600

    added more items for crafting

--- server-data/resources/salty_crafting/config.lua
+++ server-data/resources/salty_crafting/config.lua
@@ -5,14 +5,29 @@ Config.WeaponAmmo = 42
 
 Config.Recipes = {
 	-- Can be a normal ESX item
+	["lockpick"] = { 
+		{item = "rubberband", quantity = 4 }, 
+		{item = "bobbypin", quantity = 1 },
+	},
+
+	["clip"] = { 
+		{item = "gunpowder", quantity = 4 }, 
+		{item = "brass", quantity = 1 },
+	},
+
 	["diamondnecklace"] = { 
 		{item = "diamond", quantity = 4 }, 
 		{item = "silverchain", quantity = 1 },
 	},
 
-	["repairkit"] = { 
-		{item = "bobbypin", quantity = 4 }, 
-		{item = "rubberband", quantity = 1 },
+	["diamondearrings"] = { 
+		{item = "diamond", quantity = 4 }, 
+		{item = "stud", quantity = 1 },
+	},
+
+	["diamondring"] = { 
+		{item = "diamond", quantity = 4 }, 
+		{item = "ring", quantity = 1 },
 	},
 	
 	-- Can be a weapon, must follow this format
@@ -20,15 +35,31 @@ Config.Recipes = {
 		{item = "upper", quantity = 1}, 
 		{item = "lower", quantity = 1},
 		{item = "barrel", quantity = 1},
-		{item = "magazine", quantity = 1},
+		{item = "bionictrigger", quantity = 1},
 	},
 
-	['WEAPON_CARBINERIFLE'] = { 
-		{item = "upper", quantity = 1}, 
-		{item = "lower", quantity = 1},
+	['WEAPON_PUMPSHOTGUN'] = { 
+		{item = "choke", quantity = 1},
 		{item = "barrel", quantity = 1},
-		{item = "magazine", quantity = 1},
-	}
+		{item = "bionictrigger", quantity = 1},
+	},
+
+	['WEAPON_APPISTOL'] = { 
+		{item = "apbody", quantity = 1},
+		{item = "barrel", quantity = 1},
+		{item = "bionictrigger", quantity = 1},
+	},
+
+	['WEAPON_MICROSMG'] = { 
+		{item = "smgbody", quantity = 1},
+		{item = "barrel", quantity = 1},
+		{item = "bionictrigger", quantity = 1}, 
+	},
+
+	['WEAPON_PISTOL'] = { 
+		{item = "pistolbody", quantity = 1},
+		{item = "barrel", quantity = 1},
+	},
 }
 
 -- Enable a shop to access the crafting menu
