216041cfbfd07fc57eb7f2fb08becfeb65786497
Kc2693 <kailin.cannon@gmail.com>
2019-05-16 09:22:59 -0600

    do vehicle only

--- server-data/resources/[esx]/esx_advancedgarage/server/main.lua
+++ server-data/resources/[esx]/esx_advancedgarage/server/main.lua
@@ -143,8 +143,8 @@ ESX.RegisterServerCallback('esx_advancedgarage:storeVehicle', function (source, 
 		if result[1] ~= nil then
 			local originalvehprops = json.decode(result[1].vehicle)
 			if originalvehprops.model == vehiclemodel then
-				print("fuel before mysql", fuel)
-				MySQL.Async.execute('UPDATE owned_vehicles SET vehicle = @vehicle AND fuel_level = @fuel_level WHERE owner = @owner AND plate = @plate', {
+
+				MySQL.Async.execute('UPDATE owned_vehicles SET vehicle = @vehicle WHERE owner = @owner AND plate = @plate', {
 					['@owner']  = GetPlayerIdentifiers(source)[1],
 					['@vehicle'] = json.encode(vehicleProps),
 					['@plate']  = vehicleProps.plate,
