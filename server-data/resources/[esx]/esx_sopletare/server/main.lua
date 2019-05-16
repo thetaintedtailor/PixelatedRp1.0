3e8edb32b68b873d187d36bd84b3de6572bc269c
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-05-16 09:45:34 -0600

    increased chnace to find an item

--- server-data/resources/[esx]/esx_sopletare/server/main.lua
+++ server-data/resources/[esx]/esx_sopletare/server/main.lua
@@ -7,7 +7,7 @@ end)
 RegisterServerEvent('esx_sopletare:getItem')
 AddEventHandler('esx_sopletare:getItem', function()
 
-    local luck = math.random(1, 4)
+    local luck = math.random(1, 3)
 
     if luck == 1 then
 
