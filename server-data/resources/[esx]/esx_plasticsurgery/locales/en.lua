8a3dc7894b65b7139d67b7dbbea37f4980ffbfad
Kc2693 <kailin.cannon@gmail.com>
2019-05-16 05:12:02 -0600

    translation changes

--- server-data/resources/[esx]/esx_plasticsurgery/locales/en.lua
+++ server-data/resources/[esx]/esx_plasticsurgery/locales/en.lua
@@ -2,8 +2,8 @@ Locales['en'] = {
   ['valid_purchase'] = 'validate this purchase?',
   ['yes'] = 'yes',
   ['no'] = 'no',
-  ['press_access'] = 'press ~INPUT_CONTEXT~ to access Plastic Surgery',
+  ['press_access'] = 'press ~INPUT_CONTEXT~ to undergo surgery',
   ['not_enough_money'] = 'you do not have enough money',
   ['you_paid'] = 'you paid $%s',
-  ['blip_plastic_surgery'] = 'Plastic Surgery',
+  ['blip_plastic_surgery'] = 'Plastic Surgeon',
 }
