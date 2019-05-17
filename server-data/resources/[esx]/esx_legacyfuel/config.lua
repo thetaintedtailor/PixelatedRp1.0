04eca6f74fb9a34b608eeeb718d4b3b3d6b9d7e5
Kc2693 <kailin.cannon@gmail.com>
2019-05-17 09:46:43 -0600

    i dont want to do math

--- server-data/resources/[esx]/esx_legacyfuel/config.lua
+++ server-data/resources/[esx]/esx_legacyfuel/config.lua
@@ -87,15 +87,15 @@ Config.Classes = {
 
 -- The left part is at percentage RPM, and the right is how much fuel (divided by 10) you want to remove from the tank every second
 Config.FuelUsage = {
-	[1.0] = 0.2,
-	[0.9] = 0.1,
-	[0.8] = 0.09,
-	[0.7] = 0.08,
-	[0.6] = 0.07,
-	[0.5] = 0.06,
-	[0.4] = 0.05,
-	[0.3] = 0.04,
-	[0.2] = 0.03,
+	[1.0] = 0.4,
+	[0.9] = 0.3,
+	[0.8] = 0.2,
+	[0.7] = 0.1,
+	[0.6] = 0.09,
+	[0.5] = 0.08,
+	[0.4] = 0.07,
+	[0.3] = 0.06,
+	[0.2] = 0.05,
 	[0.1] = 0.02,
 	[0.0] = 0.0,
 }
