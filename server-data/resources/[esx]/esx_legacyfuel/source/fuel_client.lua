a517160344bb45815ffcb19ec476f2d7fc3daa71
Kc2693 <kailin.cannon@gmail.com>
2019-05-17 11:56:26 -0600

    adjust fuel hud

--- server-data/resources/[esx]/esx_legacyfuel/source/fuel_client.lua
+++ server-data/resources/[esx]/esx_legacyfuel/source/fuel_client.lua
@@ -459,10 +459,10 @@ if Config.EnableHUD then
 			Citizen.Wait(1)
 
 			if displayHud then
-				DrawAdvancedText(0.130 - x, 0.77 - y, 0.156, 0.0428, 0.6, mph, 255, 255, 255, 255, 6, 1)
-				DrawAdvancedText(0.174 - x, 0.77 - y, 0.156, 0.0428, 0.6, kmh, 255, 255, 255, 255, 6, 1)
-				DrawAdvancedText(0.2195 - x, 0.77 - y, 0.156, 0.0428, 0.6, fuel, 255, 255, 255, 255, 6, 1)
-				DrawAdvancedText(0.148 - x, 0.7765 - y, 0.150, 0.0428, 0.4, "mp/h              km/h                Fuel", 240, 200, 80, 255, 6, 1)
+				DrawAdvancedText(0.130 - x, 0.77 - y, 0.156, 0.0408, 0.6, mph, 255, 255, 255, 255, 6, 1)
+				DrawAdvancedText(0.174 - x, 0.77 - y, 0.156, 0.0408, 0.6, kmh, 255, 255, 255, 255, 6, 1)
+				DrawAdvancedText(0.2195 - x, 0.77 - y, 0.156, 0.0408, 0.6, fuel, 255, 255, 255, 255, 6, 1)
+				DrawAdvancedText(0.148 - x, 0.7765 - y, 0.150, 0.0408, 0.4, "mp/h              km/h                Fuel", 240, 200, 80, 255, 6, 1)
 			else
 				Citizen.Wait(750)
 			end
