82b25dcaeffe7d9a7ccce2da8ea5896706b409d8
WertyCx <jesse_anderson1993@hotmail.com>
2019-05-17 23:16:01 -0500

    change wait from 0 to 100 292 also 302/301/298

--- server-data/resources/[esx]/esx_AdvancedFishing/client.lua
+++ server-data/resources/[esx]/esx_AdvancedFishing/client.lua
@@ -289,17 +289,17 @@ end)
 Citizen.CreateThread(function()
 	while true do
 
-		Citizen.Wait(0)
+		Citizen.Wait(100)
 
 			if boatrental == false then
 
 				if (IsInVehicle()) then
 
-					if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("marquis"))  then
+					if IsVehicleModel(GetVehiclePedIsIn(playerPedId(-1), true), GetHashKey("marquis"))  then
 						DrawMarker(0, 3861.89, 4469.97, 1.50, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 3.0, 3.0, 2.0, 0, 70, 250, 30, false, true, 2, false, false, false, false)
 						if GetDistanceBetweenCoords(3861.89, 4469.97, 1.50, GetEntityCoords(LocalPed())) < 2.0 then
 							local playerPed = PlayerPedId()
-							local auto = GetVehiclePedIsIn(GetPlayerPed(-1)) 
+							local auto = GetVehiclePedIsIn(playerPedId(-1)) 
 							
 							SetTextComponentFormat('STRING');
 							AddTextComponentString("Press ~INPUT_CONTEXT~ to return ~b~rental");
