c243d9c08d3de45523256463ac6d5f2fcc5ec901
WertyCx <jesse_anderson1993@hotmail.com>
2019-05-17 22:11:05 -0500

    make boatreturn 289-319 easier to read

--- server-data/resources/[esx]/esx_AdvancedFishing/client.lua
+++ server-data/resources/[esx]/esx_AdvancedFishing/client.lua
@@ -288,26 +288,33 @@ end)
 
 Citizen.CreateThread(function()
 	while true do
+
 		Citizen.Wait(0)
-		if boatrental == false then
-			if (IsInVehicle()) then
-				if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("MARQUIS"))  then
-					DrawMarker(0, 3861.89, 4469.97, 1.50, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 3.0, 3.0, 2.0, 0, 70, 250, 30, false, true, 2, false, false, false, false)
-					if GetDistanceBetweenCoords(3861.89, 4469.97, 1.50, GetEntityCoords(LocalPed())) < 2.0 then
-						local playerPed = PlayerPedId()
-						local auto = GetVehiclePedIsIn(GetPlayerPed(-1)) 
-						SetTextComponentFormat('STRING');
-						AddTextComponentString("Press ~E~ to return ~b~rental");
-						DisplayHelpTextFromStringLabel(0, 0, 1, -1);
-						if IsPedInAnyVehicle(playerPed, false) then
-							if (IsControlJustReleased(1, 38)) then
-								ESX.Game.DeleteVehicle(auto)
+
+			if boatrental == false then
+
+				if (IsInVehicle()) then
+
+					if IsVehicleModel(GetVehiclePedIsIn(GetPlayerPed(-1), true), GetHashKey("MARQUIS"))  then
+						DrawMarker(1, 3861.89, 4469.97, 1.50, 0.0, 0.0, 0.0, 0, 0.0, 0.0, 3.0, 3.0, 2.0, 0, 70, 250, 30, false, true, 2, false, false, false, false)
+						if GetDistanceBetweenCoords(3861.89, 4469.97, 1.50, GetEntityCoords(LocalPed())) < 2.0 then
+							local playerPed = PlayerPedId()
+							local auto = GetVehiclePedIsIn(GetPlayerPed(-1)) 
+							
+							SetTextComponentFormat('STRING');
+							AddTextComponentString("Press ~'E'~ to return ~b~rental");
+							DisplayHelpTextFromStringLabel(0, 0, 1, -1);
+							
+							if IsPedInAnyVehicle(playerPed, false) then
+								if (IsControlJustReleased(1, 38)) then
+									ESX.Game.DeleteVehicle(auto)
+									
+								end
 							end
 						end
 					end
 				end
-			end
-		end	
+			end	
 	end
 end)
 
