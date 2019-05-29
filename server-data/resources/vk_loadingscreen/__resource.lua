64237d2b2314ec1e536ba63534fff7d10c6cc99d
Kc2693 <kailin.cannon@gmail.com>
2019-05-29 14:03:22 -0600

    change loading screen song

--- server-data/resources/vk_loadingscreen/__resource.lua
+++ server-data/resources/vk_loadingscreen/__resource.lua
@@ -3,7 +3,7 @@ files {
     'config.js',
     'script.js',
     'style.css',
-    'music/smug-dancin.mp3',
+    'music/subfocus_lastjungle.mp3',
     'icon/crown.png',
     'icon/discord.png',
     'icon/facebook.png',
