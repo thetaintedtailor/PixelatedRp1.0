e08c42c5dcc975ee41919c867cb091e8b200ea03
Kc2693 <kailin.cannon@gmail.com>
2019-05-29 00:33:42 -0600

    Try to suppress pd car spawn

--- server-data/resources/scrp_scripts/client/disable_dispatch-client.lua
+++ server-data/resources/scrp_scripts/client/disable_dispatch-client.lua
@@ -1,5 +1,28 @@
+local vehicles = {
+	{ hash="-475225213" }, -- police trainer
+	{ hash="666014468" }, -- police cruiser
+	{ hash="2038038420" }, -- police stanier
+	{ hash="-1537810200" }, -- police buffalo
+	{ hash="-1309990298" }, -- fbi buffalo
+	{ hash="-64097686" }, -- fbi granger
+	{ hash="-35776806" }, -- sheriff cruiser
+	{ hash="-2007311232" }, -- sheriff buffalo
+	--{ hash="-239432727" }, -- sheriff towtruck
+	{ hash="-545384401" }, -- highway cruiser
+	{ hash="-57703123" }, -- highway buffalo
+	{ hash="2005531687" }, -- highway byke
+}
+
 Citizen.CreateThread(function()
 	for i = 1, 15 do
 		EnableDispatchService(i, false)
+	end
+end)
+
+Citizen.CreateThread(function()
+	while true do
+		for k,v in pairs(vehicles) do 
+			SetVehicleModelIsSuppressed(v.hash, true)
+		end
 	end
 end)
