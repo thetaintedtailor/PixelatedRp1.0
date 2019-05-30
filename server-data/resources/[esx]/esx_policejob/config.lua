c93fec960c861009b4359fc2c91ac114a63a3d10
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-05-30 10:13:35 -0600

    increased weapon license price and gave cops the ability to revoke licenses

--- server-data/resources/[esx]/esx_policejob/config.lua
+++ server-data/resources/[esx]/esx_policejob/config.lua
@@ -9,7 +9,7 @@ Config.EnablePlayerManagement     = true
 Config.EnableArmoryManagement     = false
 Config.EnableESXIdentity          = false -- enable if you're using esx_identity
 Config.EnableNonFreemodePeds      = false -- turn this on if you want custom peds
-Config.EnableLicenses             = false -- enable if you're using esx_license
+Config.EnableLicenses             = true -- enable if you're using esx_license
 
 Config.EnableHandcuffTimer        = true -- enable handcuff timer? will unrestrain player after the time ends
 Config.HandcuffTimer              = 10 * 60000 -- 10 mins
