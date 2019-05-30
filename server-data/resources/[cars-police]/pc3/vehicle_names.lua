0f77dc6603095c5dd692fffc7e3263cae5cb6670
Kc2693 <kailin.cannon@gmail.com>
2019-05-29 22:31:19 -0600

    try to custom add police2

--- server-data/resources/[cars-police]/pc3/vehicle_names.lua
+++
@@ -1,9 +0,0 @@
-function AddTextEntry(key, value)
-	Citizen.InvokeNative(GetHashKey("ADD_TEXT_ENTRY"), key, value)
-end
-
-Citizen.CreateThread(function()
-    --Example 1: AddTextEntry('gameName', 'VEHICLE_NAME_HERE')
-    --Example 2: AddTextEntry('f350', '2013 Ford F350')
-    AddTextEntry('SRT8', 'Jeep SRT8')
-end)
