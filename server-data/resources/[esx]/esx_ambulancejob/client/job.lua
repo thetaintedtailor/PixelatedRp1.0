9d9cc50098ef051f7a7cf5ebf7d0223bc8fb456a
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-05-31 11:59:59 -0600

    added ems items to the menu

--- server-data/resources/[esx]/esx_ambulancejob/client/job.lua
+++ server-data/resources/[esx]/esx_ambulancejob/client/job.lua
@@ -904,7 +904,10 @@ function OpenPharmacyMenu()
 		align    = 'left',
 		elements = {
 			{label = _U('pharmacy_take', _U('medikit')), value = 'medikit'},
-			{label = _U('pharmacy_take', _U('bandage')), value = 'bandage'}
+			{label = _U('pharmacy_take', _U('bandage')), value = 'bandage'},
+			{label = _U('pharmacy_take', _U('morphine')), value = 'morphine'},
+			{label = _U('pharmacy_take', _U('gauze')), value = 'gauze'},
+			{label = _U('pharmacy_take', _U('mdeicaltape')), value = 'medicaltape'}
 		}
 	}, function(data, menu)
 		TriggerServerEvent('esx_ambulancejob:giveItem', data.current.value)
