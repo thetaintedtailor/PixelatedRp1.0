3ab61ba543947dd6a1fc3197188b2c8db347faed
Kc2693 <kailin.cannon@gmail.com>
2019-06-01 20:08:10 -0600

    add charger handling

---
+++ server-data/resources/[cars-police]/charger/__resource.lua
@@ -0,0 +1,15 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+data_file 'HANDLING_FILE' 'handling.meta'
+--data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
+--data_file 'CARCOLS_FILE' 'carcols.meta'
+--data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
+
+files {
+   -- 'vehicles.meta',
+   -- 'carvariations.meta',
+   -- 'carcols.meta',
+    'handling.meta',
+}
+
+--client_script 'vehicle_names.lua'
