780f8f2f1c3e790614e7e04695a69f396cb19a5d
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-06-05 09:10:29 -0600

    added new fishing mod

--- server-data/resources/[esx]/vg_fishing/databaseitems.sql
+++ server-data/resources/[esx]/vg_fishing/databaseitems.sql
@@ -4,3 +4,4 @@ INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('fi
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('shark', 'Shark', -1, 0, 1);
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('turtle', 'Sea Turtle', 3, 0, 1);
 INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('turtlebait', 'Turtle Bait', 10, 0, 1);
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES ('fishingknife', 'Fishing Knife', 1, 0, 1);
