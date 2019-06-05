ba5ded59fcc9786d7ace15cd511ed81817a8452c
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-06-04 20:03:07 -0600

    swapped drug effects to pouches

--- server-data/resources/[esx]/esx_drugeffects/server/main.lua
+++ server-data/resources/[esx]/esx_drugeffects/server/main.lua
@@ -5,41 +5,41 @@ TriggerEvent('esx:getSharedObject', function(obj)
 	ESX = obj
 end)
 
-ESX.RegisterUsableItem('weed', function(source)
+ESX.RegisterUsableItem('weed_pooch', function(source)
         
         local _source = source
 	local xPlayer = ESX.GetPlayerFromId(_source)
-	xPlayer.removeInventoryItem('weed', 1)
+	xPlayer.removeInventoryItem('weed_pooch', 1)
 
 	TriggerClientEvent('esx_status:add', _source, 'drug', 166000)
 	TriggerClientEvent('esx_drugeffects:onWeed', source)
 end)
 
-ESX.RegisterUsableItem('opium', function(source)
+ESX.RegisterUsableItem('opium_pooch', function(source)
        
         local _source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem('opium', 1)
+	xPlayer.removeInventoryItem('opium_pooch', 1)
 
 	TriggerClientEvent('esx_status:add', _source, 'drug', 249000)
 	TriggerClientEvent('esx_drugeffects:onOpium', source)
 end)
 
-ESX.RegisterUsableItem('meth', function(source)
+ESX.RegisterUsableItem('meth_pooch', function(source)
         
         local _source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem('meth', 1)
+	xPlayer.removeInventoryItem('meth_pooch', 1)
 
 	TriggerClientEvent('esx_status:add', _source, 'drug', 333000)
 	TriggerClientEvent('esx_drugeffects:onMeth', source)
 end)
 
-ESX.RegisterUsableItem('coke', function(source)
+ESX.RegisterUsableItem('coke_pooch', function(source)
         
         local _source = source
 	local xPlayer = ESX.GetPlayerFromId(source)
-	xPlayer.removeInventoryItem('coke', 1)
+	xPlayer.removeInventoryItem('coke_pooch', 1)
 
 	TriggerClientEvent('esx_status:add', _source, 'drug', 499000)
 	TriggerClientEvent('esx_drugeffects:onCoke', source)
