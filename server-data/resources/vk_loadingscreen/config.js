f45873ab1272300cf5737ced44001bb89ca0b8aa
Kc2693 <kailin.cannon@gmail.com>
2019-06-09 13:21:03 -0600

    change loading screen song

--- server-data/resources/vk_loadingscreen/config.js
+++ server-data/resources/vk_loadingscreen/config.js
@@ -38,9 +38,9 @@ VK.tips.list = [ // Tips list
 ];
 
 VK.music.volume = 0.08; // Music volume (0-1)
-VK.music.url = "music/Omnia-CYBERPUNK.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
-VK.music.title = "Omnia - CYBERPUNK"; // Music title ("NONE" to desactive)
-VK.music.submitedBy = "suggested by Katalyst (IGN: Connor OShea)"; // Music submited by... ("NONE" to desactive)
+VK.music.url = "music/JuiceWRLD_Robbery.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
+VK.music.title = "Juice WRLD - Robbery"; // Music title ("NONE" to desactive)
+VK.music.submitedBy = "suggested by Matthew (ign: Connor Carver)"; // Music submited by... ("NONE" to desactive)
 
 VK.info.logo = "https://i.postimg.cc/tCWn8Scx/pixelated.png"; // Logo ("NONE" to desactive)
 VK.info.text = "NONE"; // Bottom right corner text ("NONE" to desactive)
