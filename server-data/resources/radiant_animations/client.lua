3047ec843ddbe6c95d8f3b7a8e1e83b4a5671732
Kc2693 <kailin.cannon@gmail.com>
2019-06-13 21:47:42 -0600

    change crouch back

--- server-data/resources/radiant_animations/client.lua
+++ server-data/resources/radiant_animations/client.lua
@@ -120,30 +120,32 @@ local Keys = {["X"] = 73, ["Z"] = 20, ["SHIFT"] = 209}
 
 local crouched = false
 Citizen.CreateThread( function()
-    while true do
-        Citizen.Wait(1)
+    while true do 
+        Citizen.Wait( 1 )
 
         local ped = GetPlayerPed( -1 )
 
-        if (DoesEntityExist(ped) and not IsEntityDead(ped)) then
-            DisableControlAction( 0, 36, true ) -- INPUT_DUCK
+        if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
+            DisableControlAction( 0, 36, true ) -- INPUT_DUCK  
 
-			if ( not IsPauseMenuActive() ) then
-				RequestAnimSet( "move_ped_crouched" )
-                if ( IsDisabledControlJustPressed( 0, 36 ) ) then
-                    while ( not HasAnimSetLoaded( "move_ped_crouched" ) ) do
+            if ( not IsPauseMenuActive() ) then 
+                if ( IsDisabledControlJustPressed( 0, 36 ) ) then 
+                    RequestAnimSet( "move_ped_crouched" )
+
+                    while ( not HasAnimSetLoaded( "move_ped_crouched" ) ) do 
                         Citizen.Wait( 100 )
-                    end
-                    if ( crouched == true ) then
+                    end 
+
+                    if ( crouched == true ) then 
                         ResetPedMovementClipset( ped, 0 )
-                        crouched = false
+                        crouched = false 
                     elseif ( crouched == false ) then
                         SetPedMovementClipset( ped, "move_ped_crouched", 0.25 )
-                        crouched = true
-                    end
+                        crouched = true 
+                    end 
                 end
-            end
-        end
+            end 
+        end 
     end
 end )
 
