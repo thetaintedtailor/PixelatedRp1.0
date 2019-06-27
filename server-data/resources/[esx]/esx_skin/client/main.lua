eeccfc2dd3ea71e061260ee5d89dd08fb8b25ca9
Kc2693 <kailin.cannon@gmail.com>
2019-06-27 01:04:20 -0600

    put fov back in

--- server-data/resources/[esx]/esx_skin/client/main.lua
+++ server-data/resources/[esx]/esx_skin/client/main.lua
@@ -154,8 +154,8 @@ function CreateSkinCam()
 	RenderScriptCams(true, true, 500, true, true)
 
 	isCameraActive = true
-	SetCamRot(cam, 0.0, 0.0, 650.0, true)
-	--SetCamFov(cam, 90)
+	SetCamRot(cam, 0.0, 0.0, 200.0, true)
+	SetCamFov(cam, 130)
 	SetEntityHeading(playerPed, 90.0)
 end
 
