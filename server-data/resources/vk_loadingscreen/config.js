8131ada445afbc2c4ba91972c8c63b527924a481
Kc2693 <kailin.cannon@gmail.com>
2019-07-03 07:26:47 -0600

    change loadingscreen song

--- server-data/resources/vk_loadingscreen/config.js
+++ server-data/resources/vk_loadingscreen/config.js
@@ -37,9 +37,9 @@ VK.tips.list = [ // Tips list
 ];
 
 VK.music.volume = 0.08; // Music volume (0-1)
-VK.music.url = "music/fool_forever.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
-VK.music.title = "Forever Fool - The Harpoonist & The Axe Murderer"; // Music title ("NONE" to desactive)
-VK.music.submitedBy = "suggested by Indigojar (IGN: Jimmy Lee)"; // Music submited by... ("NONE" to desactive)
+VK.music.url = "music/CarsonHayes_Miami.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
+VK.music.title = "Carson Hayes & SaneBeats - Miami"; // Music title ("NONE" to desactive)
+VK.music.submitedBy = "suggested by A-101 Chief Bender"; // Music submited by... ("NONE" to desactive)
 
 VK.info.logo = "https://i.postimg.cc/cL86JYLL/pixelatedlogo2.png"; // Logo ("NONE" to desactive)
 VK.info.text = "NONE"; // Bottom right corner text ("NONE" to desactive)
