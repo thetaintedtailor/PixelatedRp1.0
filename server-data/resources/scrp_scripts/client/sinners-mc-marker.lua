3a99b68087506fdb05432b1b041a0768be5d7e17
Kc2693 <kailin.cannon@gmail.com>
2019-07-06 15:55:07 -0600

    set blip priority

--- server-data/resources/scrp_scripts/client/sinners-mc-marker.lua
+++ server-data/resources/scrp_scripts/client/sinners-mc-marker.lua
@@ -1,5 +1,6 @@
 Citizen.CreateThread(function()
-	local blip = AddBlipForCoord(982.40, -103.50, 74.85)
+    local blip = AddBlipForCoord(982.40, -103.50, 74.85)
+        SetBlipPriority(blip, 1)
 	    SetBlipSprite (blip, 494)
 		SetBlipDisplay(blip, 4)
 		SetBlipScale  (blip, 1.2)
