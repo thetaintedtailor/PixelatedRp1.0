3ad9aca2e26b6f37e3e2b2bde6d3689a1dde1b4c
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-07-10 20:19:50 -0600

    added gsr mods

---
+++ server-data/resources/Radiant_gsrtest/config.lua
@@ -0,0 +1,5 @@
+Config                          = {}
+Config.Locale                   = 'en'
+
+Config.waterClean               = true -- Set to false if you dont want water to clean off GSR from people who shot
+Config.GsrTime                  = 60 * 60 -- Change The first number if you want the GSR to be auto removed faster output is minutes.
---
+++ server-data/resources/Radiant_gsrtest/gsr.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `gsr` (
+  `identifier` varchar(250) NOT NULL,
+  `time` int(11) NOT NULL,
+  PRIMARY KEY (`identifier`)
+);
