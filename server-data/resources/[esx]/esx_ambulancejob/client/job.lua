84131c1193d16af336a81f94fb19df7b6f9a5a2e
Kc2693 <kailin.cannon@gmail.com>
2019-07-15 02:53:15 -0600

    Revert "remove print"
    This reverts commit 65fa2828e9460d71b19c1281743e828dd0fda5b2.

--- server-data/resources/[esx]/esx_ambulancejob/client/job.lua
+++ server-data/resources/[esx]/esx_ambulancejob/client/job.lua
@@ -505,6 +505,7 @@ function OpenVehicleSpawnerMenu(hospital, partNum)
 						else
 							label = label .. ('<span style="color:darkred;">%s</span>'):format(_U('garage_notstored'))
 						end
+						print('inside retrieve veh loop', v.fuel_level)
 						table.insert(garage, {
 							label = label,
 							stored = v.stored,
