cee11cd989b03abac75be536ede906655a1b57e1
Brandon R <brandon@blrice.net>
2019-07-17 16:49:40 -0400

    Add initial toko and radio menu plugins and basic config.

---
+++ server-data/resources/tokovoip_script/s_config.lua
@@ -0,0 +1,8 @@
+TokoVoipConfig = {
+	channels = {
+		{name = "Call PD Frequency 1", subscribers = {}},
+		{name = "EMS Frequency 1", subscribers = {}},
+		{name = "PD Frequency 2", subscribers = {}},
+		{name = "EMS Frequency 2", subscribers = {}},
+	}
+};
