2e07e8a4a255b06aa631b15b5ca7176af947e65b
Kc2693 <kailin.cannon@gmail.com>
2019-07-19 23:20:17 -0600

    change loadingscreen music

--- server-data/resources/vk_loadingscreen/__resource.lua
+++ server-data/resources/vk_loadingscreen/__resource.lua
@@ -3,7 +3,7 @@ files {
     'config.js',
     'script.js',
     'style.css',
-    'music/blackskinhead_kanye.mp3',
+    'music/what_you_promised_fairlane.mp3',
     'icon/crown.png',
     'icon/discord.png',
     'icon/facebook.png',
