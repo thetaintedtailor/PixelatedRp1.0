6b18fd0086857aadd60ac24c45971210df918fc9
thetaintedtailor <49380054+thetaintedtailor@users.noreply.github.com>
2019-07-24 23:38:03 -0600

    removed "blackmoney"

--- server-data/resources/[esx]/esx_burglary/config.lua
+++ server-data/resources/[esx]/esx_burglary/config.lua
@@ -1,6 +1,6 @@
 Config                = {}
 
-Config.GiveBlack      = true -- If it should give black money
+Config.GiveBlack      = false -- If it should give black money
 Config.AlertPolice    = true -- If the police should be alerted if someone fails a burglary
 
 Config.DiscordWebhook = false
