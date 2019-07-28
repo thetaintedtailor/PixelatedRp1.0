ec54512b673d05513412cccdd03b2a053da8fc3d
Kc2693 <kailin.cannon@gmail.com>
2019-07-28 13:04:40 -0600

    add radio, try an example

---
+++ server-data/resources/radio/__resource.lua
@@ -0,0 +1,17 @@
+resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
+
+-- Example custom radios
+supersede_radio "RADIO_02_POP" { url = "http://jiggaradio.com:10080/play/nightlight.ogg", volume = 0.2 }
+supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 }
+supersede_radio "RADIO_17_FUNK" { url = "http://s01.diazol.hu:35000/live.ogg", volume = 0.2 }
+
+files {
+	"index.html"
+}
+
+ui_page "index.html"
+
+client_scripts {
+	"data.js",
+	"client.js"
+}
