d0c11126930fccf873cf92bfa06fd6af3c57fe8a
Kc2693 <kailin.cannon@gmail.com>
2019-07-28 13:22:00 -0600

    Try a different radio

--- server-data/resources/radio/__resource.lua
+++ server-data/resources/radio/__resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
-supersede_radio "RADIO_02_POP" { url = "http://jiggaradio.com:10080/play/nightlight.ogg", volume = 0.2 }
+supersede_radio "RADIO_02_POP" { url = "https://tunein.com/embed/player/s111382?autoplay=true", volume = 0.2 }
 supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 }
 supersede_radio "RADIO_17_FUNK" { url = "http://s01.diazol.hu:35000/live.ogg", volume = 0.2 }
 
