8d859745b124e1f99e815fa94bc788a4c97202a6
Kc2693 <kailin.cannon@gmail.com>
2019-07-28 13:45:50 -0600

    howlerjs should support mp3

--- server-data/resources/radio/__resource.lua
+++ server-data/resources/radio/__resource.lua
@@ -1,9 +1,13 @@
 resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
-supersede_radio "RADIO_02_POP" { url = "https://tunein.com/embed/player/s111382?autoplay=true", volume = 0.2 }
+supersede_radio "RADIO_01_CLASS_ROCK" {url = "http://webradio.antennevorarlberg.at/classicrock", volume = 0.2}
+supersede_radio "RADIO_02_POP" { url = "http://webradio.antennevorarlberg.at/hits", volume = 0.2 }
 supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://stream.radioreklama.bg/nrj.ogg", volume = 0.2 }
+
 supersede_radio "RADIO_17_FUNK" { url = "http://s01.diazol.hu:35000/live.ogg", volume = 0.2 }
+supersede_radio "RADIO_13_JAZZ" { url = "http://webradio.antennevorarlberg.at/partymix", volume = 0.2}
+
 
 files {
 	"index.html"
