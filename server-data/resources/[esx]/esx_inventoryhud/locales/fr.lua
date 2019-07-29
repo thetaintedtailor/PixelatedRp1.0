2a7cae3507e7650c24690d6f9627f09e0e0e8b7f
Kc2693 <kailin.cannon@gmail.com>
2019-07-29 13:15:18 -0600

    replace old version with new

--- server-data/resources/[esx]/esx_inventoryhud/locales/fr.lua
+++ server-data/resources/[esx]/esx_inventoryhud/locales/fr.lua
@@ -1,11 +1,11 @@
-Locales['fr'] = {
-	['cash'] = 'Éspèces',
-	['player_nearby'] = 'Le joueur sélectionné n\'est plus près de vous.',
-	['players_nearby'] = 'Il n\'y a personne près de vous.',
-	['no_permission'] = 'Insufficient permissions!',
-	['generic_argument_name'] = 'Player ID',
-	['generic_argument_help'] = 'Player ID',
-	['openinventory_help'] = 'Opens player inventory',
-	['player_not_online'] = 'Player with given ID is not online',
-	['id_not_number'] = 'Incorrect player ID'
+Locales["fr"] = {
+	["cash"] = "Éspèces",
+	["black_money"] = "Argent sale",
+	["player_nearby"] = "Le joueur sélectionné n'est plus près de vous.",
+	["players_nearby"] = "Il n'y a personne près de vous.",
+	["openinv_help"] = "Opens inventory of another player",
+	["openinv_id"] = "Player ID",
+	["no_permissions"] = "You don't have permissions to do that!",
+	["no_player"] = "Didn't find player with given ID!",
+	["player_inventory"] = "Player inventory"
 }
