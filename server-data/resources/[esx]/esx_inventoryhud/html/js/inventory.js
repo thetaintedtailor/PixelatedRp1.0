a8fa2735204203d230a8943e9b1b192e17add49e
Kc2693 <kailin.cannon@gmail.com>
2019-07-29 18:18:38 -0600

    wipe second inventory for a blank repopulate

--- server-data/resources/[esx]/esx_inventoryhud/html/js/inventory.js
+++ server-data/resources/[esx]/esx_inventoryhud/html/js/inventory.js
@@ -106,12 +106,12 @@ function inventorySetup(items) {
 function secondInventorySetup(items) {
     $('.inventory').removeClass('inventory-single');
     $('.inventory').addClass('inventory-double');
+    $("#otherInventory").html("");
     $("#otherInventory").removeClass("hideSecondInventory");
     $.each(items, function (index, item) {
         count = setCount(item);
 
-        $("#otherInventory").append('<div class="slot-wrapper"><div class="slot"><div id="itemOther-' + index + '" class="item" style = "background-image: url(\'img/items/' + item.name + '.png\')">' +
-            '<div class="item-count">' + count + '</div> <div class="item-name">' + item.label + '</div> </div ><div class="item-name-bg"></div></div></div>');
+        $("#otherInventory").append('<div class="slot-wrapper"><div class="slot"><div id="itemOther-' + index + '" class="item" style = "background-image: url(\'img/items/' + item.name + '.png\')">' + '<div class="item-count">' + count + '</div> <div class="item-name">' + item.label + '</div> </div ><div class="item-name-bg"></div></div></div>');
         $('#itemOther-' + index).data('item', item);
         $('#itemOther-' + index).data('inventory', "second");
     });
