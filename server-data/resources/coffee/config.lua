54f8893286338386b8af88b78e4767de3a2597ca
Katalyst <mmcmullen07@hotmail.com>
2019-08-05 22:34:09 -0600

    New Coffee machines for PD and PB

---
+++ server-data/resources/coffee/config.lua
@@ -0,0 +1,10 @@
+Config = {}
+
+Config.Price = 5
+Config.MaxQuantity = 5
+
+
+Config.Locations = {
+    { x = 436.144, y = -985.824, z = 30.6896 },   -- lspd
+    { x = 313.67,  y = -588.15,  z = 43.29   }    -- pillbox
+}
