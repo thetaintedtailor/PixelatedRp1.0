ce08c887dba0ae784ca975ef5315b6fad1800734
open_mailbox <dev@open-mailbox.com>
2019-08-14 14:54:02 -0400

    Restore jetskirace.

---
+++ server-data/resources/jetskirace/server/main.lua
@@ -0,0 +1,30 @@
+local activeRace = false
+
+function Timer()
+	Citizen.CreateThread(function()
+		local timer = 0
+		repeat
+			timer = timer + 1
+			Citizen.Wait(1000)
+		until timer == Config.Duration or activeRace == false
+			TriggerClientEvent('jetskirace:end', -1)
+			activeRace = false
+	end)
+end
+
+RegisterServerEvent('jetskirace:start')
+AddEventHandler('jetskirace:start', function(count)
+	if not activeRace then
+		activeRace = true
+		Timer()
+		TriggerClientEvent('jetskirace:start', -1, count)
+	else
+		TriggerClientEvent('jetskirace:alreadyActive', source)
+	end
+end)
+
+RegisterServerEvent('jetskirace:end')
+AddEventHandler('jetskirace:end', function()
+	activeRace = false
+	TriggerClientEvent('jetskirace:end', -1)
+end)
