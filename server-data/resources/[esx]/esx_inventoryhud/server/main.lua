a6fef24558971f06e8dc6cc3cdd04b6a5c1b7d08
Kc2693 <kailin.cannon@gmail.com>
2019-08-17 23:51:07 -0600

    change

--- server-data/resources/[esx]/esx_inventoryhud/server/main.lua
+++ server-data/resources/[esx]/esx_inventoryhud/server/main.lua
@@ -60,17 +60,28 @@ AddEventHandler(
 )
 
 TriggerEvent('es:addGroupCommand', 'openinventory', "admin", function(source, args, user)
-	if args[1] then
-		local xPlayer = ESX.GetPlayerFromId(args[1])
+	local target = tonumber(args[1])
+	local targetXPlayer = ESX.GetPlayerFromId(target)
 
-		if xPlayer then
-			TriggerClientEvent("esx_inventoryhud:openPlayerInventory", source, xPlayer)
+		if targetXPlayer ~= nil then
+			TriggerClientEvent("esx_inventoryhud:openPlayerInventory", source, target, targetXPlayer.name)
 		else
-			TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, _U('player_not_online'))
+			TriggerClientEvent("chatMessage", source, "^1" .. _U("no_player"))
 		end
-	else
-		TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, _U('id_not_number'))
-	end
-end, function(source, args, user)
-	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, _U('no_permission'))
 end)
+
+-- TriggerEvent('es:addGroupCommand', 'openinventory', "admin", function(source, args, user)
+-- 	if args[1] then
+-- 		local xPlayer = ESX.GetPlayerFromId(args[1])
+
+-- 		if xPlayer then
+-- 			TriggerClientEvent("esx_inventoryhud:openPlayerInventory", source, xPlayer)
+-- 		else
+-- 			TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, _U('player_not_online'))
+-- 		end
+-- 	else
+-- 		TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, _U('id_not_number'))
+-- 	end
+-- end, function(source, args, user)
+-- 	TriggerClientEvent('chatMessage', source, "SYSTEM", {255, 0, 0}, _U('no_permission'))
+-- end)
