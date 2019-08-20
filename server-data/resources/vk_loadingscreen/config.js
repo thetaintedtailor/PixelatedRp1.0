fd8537db36e60a234376b6b834c273ba2084e56f
Kc2693 <kailin.cannon@gmail.com>
2019-08-20 13:57:55 -0600

    change song

--- server-data/resources/vk_loadingscreen/config.js
+++ server-data/resources/vk_loadingscreen/config.js
@@ -37,9 +37,9 @@ VK.tips.list = [ // Tips list
 ];
 
 VK.music.volume = 0.08; // Music volume (0-1)
-VK.music.url = "music/afterlife.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
-VK.music.title = "Afterlife - Avenged Sevenfold"; // Music title ("NONE" to desactive)
-VK.music.submitedBy = "Suggested by B-204 SGT Danks Komo"; // Music submited by... ("NONE" to desactive)
+VK.music.url = "music/moneyinthegrave.mp3"; // Music url, can be on local or distant (http://....) ("NONE" to desactive music)
+VK.music.title = "Money in the Grave - Drake"; // Music title ("NONE" to desactive)
+VK.music.submitedBy = "Suggested by Envy"; // Music submited by... ("NONE" to desactive)
 
 VK.info.logo = "https://i.postimg.cc/cL86JYLL/pixelatedlogo2.png"; // Logo ("NONE" to desactive)
 VK.info.text = "NONE"; // Bottom right corner text ("NONE" to desactive)
