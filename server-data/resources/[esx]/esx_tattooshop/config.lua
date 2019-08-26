2859ff4f9421154ed590e3e5ddcc2885016b6426
Katalyst <mmcmullen07@hotmail.com>
2019-08-26 00:52:13 -0600

    Fix for tattooshop errors in console spam

--- server-data/resources/[esx]/esx_tattooshop/config.lua
+++ server-data/resources/[esx]/esx_tattooshop/config.lua
@@ -1,8 +1,8 @@
 Config              = {}
 Config.DrawDistance = 100
 Config.Size         = {x = 1.5, y = 1.5, z = 1.5}
---Config.Color        = {r = 0, g = 128, b = 255}
-Config.Type         = 1
+Config.Color        = {r = 0, g = 128, b = 255}
+Config.Type         = 23
 Config.Locale       = 'en'
 
 Config.Zones = {
