780e039d34b9b679271a9f7c86c4e010b653bf7a
Kc2693 <kailin.cannon@gmail.com>
2019-08-27 15:34:37 -0600

    change song

--- server-data/resources/vk_loadingscreen/__resource.lua
+++ server-data/resources/vk_loadingscreen/__resource.lua
@@ -3,7 +3,7 @@ files {
     'config.js',
     'script.js',
     'style.css',
-    'music/dixieland_delight.mp3',
+    'music/hold_my_hand.mp3',
     'icon/crown.png',
     'icon/discord.png',
     'icon/facebook.png',
