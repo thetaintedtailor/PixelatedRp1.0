61caec581ba0229beb4776c7b6f8be4e25d21814
allupinthat <52768065+allupinthat@users.noreply.github.com>
2019-09-09 01:11:24 -0400

    Add hyperstun and beanbag resources to  staging

---
+++ server-data/resources/beanbag/__resource.lua
@@ -0,0 +1,12 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+
+description "Weapons Add-on"
+
+files {
+    'data/weapons.meta',
+
+}
+
+data_file 'WEAPON_METADATA_FILE' 'data/weapons.meta'
+data_file 'WEAPONINFO_FILE' 'data/weapons.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons.meta'
