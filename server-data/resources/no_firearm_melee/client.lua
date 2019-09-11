4c07eb72a46e067ba0604b5d0920a101d0ee6a8e
Katalyst <mmcmullen07@hotmail.com>
2019-09-10 23:49:11 -0600

    Fix it. it's broken. Now it's not.

--- server-data/resources/no_firearm_melee/client.lua
+++ server-data/resources/no_firearm_melee/client.lua
@@ -1,11 +1,7 @@
 Citizen.CreateThread(function()
-	local ped = PlayerPedId()
     while true do
 		Citizen.Wait(0)
-		if ped == nil then
-			ped = PlayerPedId()
-		end
-
+		local ped = PlayerPedId()
         if IsPedArmed(ped, 6) then
 	    	DisableControlAction(1, 140, true)
             DisableControlAction(1, 141, true)
