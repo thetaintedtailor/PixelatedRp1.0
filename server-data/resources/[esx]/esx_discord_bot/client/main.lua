9c1f6ae53c0233743b6514cd65b0511e856de2bf
open_mailbox <dev@open-mailbox.com>
2019-09-15 12:08:20 -0400

    Try alternate method of getting player's killer.

--- server-data/resources/[esx]/esx_discord_bot/client/main.lua
+++ server-data/resources/[esx]/esx_discord_bot/client/main.lua
@@ -2,16 +2,16 @@ ESX                           = nil
 local PlayerData       = {}
 
 Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
+    while ESX == nil do
+        TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+        Citizen.Wait(0)
+    end
 end)
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
   PlayerData = xPlayer
-	TriggerServerEvent("esx:playerconnected")
+    TriggerServerEvent("esx:playerconnected")
 
 
 end)
@@ -24,91 +24,91 @@ end)
 local isJacking = true
 local isStolen = true
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
+    while true do
+        Citizen.Wait(0)
 
-		if(IsPedInAnyVehicle(GetPlayerPed(-1)))then
-			local playerPed = GetPlayerPed(-1)
-			local coords    = GetEntityCoords(playerPed)
-			vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 7.0, 0, 70)
-			if(IsVehicleStolen(vehicle) and isStolen )then
-				Wait(1000)
-				TriggerServerEvent("esx:jackingcar",GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
-				isStolen = false
-			end
-		else
-			isStolen = true
-			vehicle = nil
-		end
+        if(IsPedInAnyVehicle(GetPlayerPed(-1)))then
+            local playerPed = GetPlayerPed(-1)
+            local coords    = GetEntityCoords(playerPed)
+            vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 7.0, 0, 70)
+            if(IsVehicleStolen(vehicle) and isStolen )then
+                Wait(1000)
+                TriggerServerEvent("esx:jackingcar",GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
+                isStolen = false
+            end
+        else
+            isStolen = true
+            vehicle = nil
+        end
 
-		if(IsPedJacking(GetPlayerPed(-1))) then
-				if(settings.LogPedJacking == true and isJacking) then
-					local playerPed = GetPlayerPed(-1)
-					local coords    = GetEntityCoords(playerPed)
+        if(IsPedJacking(GetPlayerPed(-1))) then
+                if(settings.LogPedJacking == true and isJacking) then
+                    local playerPed = GetPlayerPed(-1)
+                    local coords    = GetEntityCoords(playerPed)
 
-					local vehicle = nil
+                    local vehicle = nil
 
-					if IsPedInAnyVehicle(playerPed) then
-						vehicle = GetVehiclePedIsIn(playerPed)
-					else
-						vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 7.0, 0, 70)
-					end
-					Wait(1000)
-					TriggerServerEvent("esx:jackingcar",GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
+                    if IsPedInAnyVehicle(playerPed) then
+                        vehicle = GetVehiclePedIsIn(playerPed)
+                    else
+                        vehicle = GetClosestVehicle(coords.x, coords.y, coords.z, 7.0, 0, 70)
+                    end
+                    Wait(1000)
+                    TriggerServerEvent("esx:jackingcar",GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
 
-					isJacking = false
-					vehicle = nil
-				end
-		else
-			isJacking = true
-		end
-	end
+                    isJacking = false
+                    vehicle = nil
+                end
+        else
+            isJacking = true
+        end
+    end
 end)
 
 local isIncarPolice = false
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
+    while true do
+        Citizen.Wait(0)
 
-		if(IsPedInAnyPoliceVehicle(GetPlayerPed(-1))) then
-				if(settings.LogEnterPoliceVehicle == true and not isIncarPolice and PlayerData.job.name ~= "police") then
-					TriggerServerEvent("esx:enterpolicecar",GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsIn(GetPlayerPed(-1), 0))))
-					isIncarPolice = true
-				end
-		else
-			isIncarPolice = false
-		end
-	end
+        if(IsPedInAnyPoliceVehicle(GetPlayerPed(-1))) then
+                if(settings.LogEnterPoliceVehicle == true and not isIncarPolice and PlayerData.job.name ~= "police") then
+                    TriggerServerEvent("esx:enterpolicecar",GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsIn(GetPlayerPed(-1), 0))))
+                    isIncarPolice = true
+                end
+        else
+            isIncarPolice = false
+        end
+    end
 end)
 
 local isIncar = false
 Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(0)
+    while true do
+        Citizen.Wait(0)
 
-		if(IsPedInAnyVehicle(GetPlayerPed(-1)) and not IsPedInAnyPoliceVehicle(GetPlayerPed(-1))) then
+        if(IsPedInAnyVehicle(GetPlayerPed(-1)) and not IsPedInAnyPoliceVehicle(GetPlayerPed(-1))) then
 
-				if(settings.LogEnterPoliceVehicle == true and not isIncar) then
+                if(settings.LogEnterPoliceVehicle == true and not isIncar) then
 
-					for i=1, #blacklistedModels, 1 do
+                    for i=1, #blacklistedModels, 1 do
 
-						if(blacklistedModels[i] == GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsIn(GetPlayerPed(-1), 0))))then
-							TriggerServerEvent("esx:enterblacklistedcar",GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsIn(GetPlayerPed(-1), 0))))
-							isIncar = true
-						end
-					end
-				end
-		else
-			isIncar = false
-		end
-	end
+                        if(blacklistedModels[i] == GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsIn(GetPlayerPed(-1), 0))))then
+                            TriggerServerEvent("esx:enterblacklistedcar",GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsIn(GetPlayerPed(-1), 0))))
+                            isIncar = true
+                        end
+                    end
+                end
+        else
+            isIncar = false
+        end
+    end
 end)
 
 local base = 0
 Citizen.CreateThread(function()
     local isDead = false
     local hasBeenDead = false
-	local diedAt
+    local diedAt
 
     while true do
         Wait(0)
@@ -121,29 +121,33 @@ Citizen.CreateThread(function()
             if IsPedFatallyInjured(ped) and not isDead then
                 isDead = true
                 if not diedAt then
-                	diedAt = GetGameTimer()
+                    diedAt = GetGameTimer()
                 end
 
                 local killer, killerweapon = NetworkGetEntityKillerOfPlayer(player)
-				local killerentitytype = GetEntityType(killer)
-				local killertype = -1
-				local killerinvehicle = false
-				local killervehiclename = ''
-                local killervehicleseat = 0
-				if killerentitytype == 1 then
-					killertype = GetPedType(killer)
-					if IsPedInAnyVehicle(killer, false) == 1 then
-						killerinvehicle = true
-						killervehiclename = GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(killer)))
-                        killervehicleseat = GetPedVehicleSeat(killer)
-					else killerinvehicle = false
-					end
-				end
+                local source               = GetPedSourceOfDeath(ped)
 
-				local killerid = GetPlayerByEntityID(killer)
-				if killer ~= ped and killerid ~= nil and NetworkIsPlayerActive(killerid) then killerid = GetPlayerServerId(killerid)
-				else killerid = -1
-				end
+                killer = killer or source
+
+                local killerentitytype = GetEntityType(killer)
+                local killertype = -1
+                local killerinvehicle = false
+                local killervehiclename = ''
+                local killervehicleseat = 0
+                if killerentitytype == 1 then
+                    killertype = GetPedType(killer)
+                    if IsPedInAnyVehicle(killer, false) == 1 then
+                        killerinvehicle = true
+                        killervehiclename = GetDisplayNameFromVehicleModel(GetEntityModel(GetVehiclePedIsUsing(killer)))
+                        killervehicleseat = GetPedVehicleSeat(killer)
+                    else killerinvehicle = false
+                    end
+                end
+
+                local killerid = GetPlayerByEntityID(killer)
+                if killer ~= ped and killerid ~= nil and NetworkIsPlayerActive(killerid) then killerid = GetPlayerServerId(killerid)
+                else killerid = -1
+                end
 
                 if killer == ped or killer == -1 then
                     TriggerEvent('esx:killerlog',0, killertype, { table.unpack(GetEntityCoords(ped)) })
@@ -163,8 +167,8 @@ Citizen.CreateThread(function()
 end)
 
 function GetPlayerByEntityID(id)
-	for i=0,256 do
-		if(NetworkIsPlayerActive(i) and GetPlayerPed(i) == id) then return i end
-	end
-	return nil
+    for i=0,256 do
+        if(NetworkIsPlayerActive(i) and GetPlayerPed(i) == id) then return i end
+    end
+    return nil
 end
