10d8f36630b05c44c7746ff91b52969427adb593
open_mailbox <dev@open-mailbox.com>
2019-09-16 16:48:16 -0400

    Register openinventory using es superadmin permissions.

--- server-data/resources/[esx]/esx_inventoryhud/server/main.lua
+++ server-data/resources/[esx]/esx_inventoryhud/server/main.lua
@@ -1,78 +1,93 @@
 ESX = nil
 
 TriggerEvent(
-	"esx:getSharedObject",
-	function(obj)
-		ESX = obj
-	end
+    "esx:getSharedObject",
+    function(obj)
+        ESX = obj
+    end
 )
 
 ESX.RegisterServerCallback(
-	"esx_inventoryhud:getPlayerInventory",
-	function(source, cb, target)
-		local targetXPlayer = ESX.GetPlayerFromId(target)
+    "esx_inventoryhud:getPlayerInventory",
+    function(source, cb, target)
+        local targetXPlayer = ESX.GetPlayerFromId(target)
 
-		if targetXPlayer ~= nil then
-			cb({inventory = targetXPlayer.inventory, money = targetXPlayer.getMoney(), accounts = targetXPlayer.accounts, weapons = targetXPlayer.loadout})
-		else
-			cb(nil)
-		end
-	end
+        if targetXPlayer ~= nil then
+            cb({inventory = targetXPlayer.inventory, money = targetXPlayer.getMoney(), accounts = targetXPlayer.accounts, weapons = targetXPlayer.loadout})
+        else
+            cb(nil)
+        end
+    end
 )
 
 RegisterServerEvent("esx_inventoryhud:tradePlayerItem")
 AddEventHandler(
-	"esx_inventoryhud:tradePlayerItem",
-	function(from, target, type, itemName, itemCount)
-		local _source = from
+    "esx_inventoryhud:tradePlayerItem",
+    function(from, target, type, itemName, itemCount)
+        local _source = from
 
-		local sourceXPlayer = ESX.GetPlayerFromId(_source)
-		local targetXPlayer = ESX.GetPlayerFromId(target)
+        local sourceXPlayer = ESX.GetPlayerFromId(_source)
+        local targetXPlayer = ESX.GetPlayerFromId(target)
 
-		if type == "item_standard" then
-			local sourceItem = sourceXPlayer.getInventoryItem(itemName)
-			local targetItem = targetXPlayer.getInventoryItem(itemName)
+        if type == "item_standard" then
+            local sourceItem = sourceXPlayer.getInventoryItem(itemName)
+            local targetItem = targetXPlayer.getInventoryItem(itemName)
 
-			if itemCount > 0 and sourceItem.count >= itemCount then
-				if targetItem.limit ~= -1 and (targetItem.count + itemCount) > targetItem.limit then
-				else
-					sourceXPlayer.removeInventoryItem(itemName, itemCount)
-					targetXPlayer.addInventoryItem(itemName, itemCount)
-				end
-			end
-		elseif type == "item_money" then
-			if itemCount > 0 and sourceXPlayer.getMoney() >= itemCount then
-				sourceXPlayer.removeMoney(itemCount)
-				targetXPlayer.addMoney(itemCount)
-			end
-		elseif type == "item_account" then
-			if itemCount > 0 and sourceXPlayer.getAccount(itemName).money >= itemCount then
-				sourceXPlayer.removeAccountMoney(itemName, itemCount)
-				targetXPlayer.addAccountMoney(itemName, itemCount)
-			end
-		elseif type == "item_weapon" then
-			if not targetXPlayer.hasWeapon(itemName) then
-				sourceXPlayer.removeWeapon(itemName)
-				targetXPlayer.addWeapon(itemName, itemCount)
-			end
-		end
-	end
+            if itemCount > 0 and sourceItem.count >= itemCount then
+                if targetItem.limit ~= -1 and (targetItem.count + itemCount) > targetItem.limit then
+                else
+                    sourceXPlayer.removeInventoryItem(itemName, itemCount)
+                    targetXPlayer.addInventoryItem(itemName, itemCount)
+                end
+            end
+        elseif type == "item_money" then
+            if itemCount > 0 and sourceXPlayer.getMoney() >= itemCount then
+                sourceXPlayer.removeMoney(itemCount)
+                targetXPlayer.addMoney(itemCount)
+            end
+        elseif type == "item_account" then
+            if itemCount > 0 and sourceXPlayer.getAccount(itemName).money >= itemCount then
+                sourceXPlayer.removeAccountMoney(itemName, itemCount)
+                targetXPlayer.addAccountMoney(itemName, itemCount)
+            end
+        elseif type == "item_weapon" then
+            if not targetXPlayer.hasWeapon(itemName) then
+                sourceXPlayer.removeWeapon(itemName)
+                targetXPlayer.addWeapon(itemName, itemCount)
+            end
+        end
+    end
 )
 
+TriggerEvent('es:addGroupCommand', 'openinventory', "superadmin", function(source, args, user)
+    local target        = tonumber(args[1])
+    local targetXPlayer = ESX.GetPlayerFromId(target)
+
+    if targetXPlayer ~= nil then
+        TriggerClientEvent("esx_inventoryhud:openPlayerInventory", source, target, targetXPlayer.name)
+    else
+        TriggerClientEvent("chatMessage", source, "^1" .. _U("no_player"))
+    end
+end, function(source, args, user)
+    TriggerClientEvent('chat:addMessage', source, { args = {"^1SYSTEM", "Insufficienct permissions!"} })
+end, {help = "Open a user's inventory", params = {{name = "userid", help = "The ID of the player"}}})
+
+--[[
 RegisterCommand(
-	"openinventory",
-	function(source, args, rawCommand)
-		if IsPlayerAceAllowed(source, "inventory.openinventory") then
-			local target = tonumber(args[1])
-			local targetXPlayer = ESX.GetPlayerFromId(target)
+    "openinventory",
+    function(source, args, rawCommand)
+        if IsPlayerAceAllowed(source, "inventory.openinventory") then
+            local target = tonumber(args[1])
+            local targetXPlayer = ESX.GetPlayerFromId(target)
 
-			if targetXPlayer ~= nil then
-				TriggerClientEvent("esx_inventoryhud:openPlayerInventory", source, target, targetXPlayer.name)
-			else
-				TriggerClientEvent("chatMessage", source, "^1" .. _U("no_player"))
-			end
-		else
-			TriggerClientEvent("chatMessage", source, "^1" .. _U("no_permissions"))
-		end
-	end
+            if targetXPlayer ~= nil then
+                TriggerClientEvent("esx_inventoryhud:openPlayerInventory", source, target, targetXPlayer.name)
+            else
+                TriggerClientEvent("chatMessage", source, "^1" .. _U("no_player"))
+            end
+        else
+            TriggerClientEvent("chatMessage", source, "^1" .. _U("no_permissions"))
+        end
+    end
 )
+]]
