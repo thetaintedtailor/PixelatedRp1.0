94773572e6a15c5ae96c6966f9ae9862df7d7d1f
Kc2693 <kailin.cannon@gmail.com>
2019-09-23 14:34:58 -0600

    add coffee machines

--- server-data/resources/coffeemachines/config.lua
+++ server-data/resources/coffeemachines/config.lua
@@ -6,5 +6,11 @@ Config.MaxQuantity = 5
 
 Config.Locations = {
     { x = 436.144, y = -985.824, z = 30.6896 },   -- lspd
-    { x = 313.67,  y = -588.15,  z = 43.29   }    -- pillbox
+    { x = 313.67,  y = -588.15,  z = 43.29   },    -- pillbox
+    { x = -38.18,  y = -1094.48,  z = 26.42 }, -- dealership
+
+    -- expanded PD coffee machines
+    { x = 452.86,  y = -987.89,  z = 26.67 },
+    { x = 470.25,  y = -982.46,  z = 24.91 },
+    { x = 430.24,  y = -997.32,  z = 34.74 },
 }
